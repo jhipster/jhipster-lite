7dcbcd977a35893b2675086a612704d6a9b0ed4b
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-07-23 12:13:47 +0200

    Add Spring Boot

---
+++ src/main/java/tech/jhipster/forge/JhforgeApp.java
@@ -0,0 +1,51 @@
+package tech.jhipster.forge;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class JhforgeApp {
+
+  private static final Logger log = LoggerFactory.getLogger(JhforgeApp.class);
+
+  public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(JhforgeApp.class);
+    Environment env = app.run(args).getEnvironment();
+    logApplicationStartup(env);
+  }
+
+  private static void logApplicationStartup(Environment env) {
+    String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
+    String serverPort = env.getProperty("server.port");
+    String contextPath = Optional.ofNullable(env.getProperty("server.servlet.context-path")).filter(StringUtils::isNotBlank).orElse("/");
+    String hostAddress = "localhost";
+    try {
+      hostAddress = InetAddress.getLocalHost().getHostAddress();
+    } catch (UnknownHostException e) {
+      log.warn("The host name could not be determined, using `localhost` as fallback");
+    }
+    log.info(
+      "\n----------------------------------------------------------\n\t" +
+      "Application '{}' is running! Access URLs:\n\t" +
+      "Local: \t\t{}://localhost:{}{}\n\t" +
+      "External: \t{}://{}:{}{}\n\t" +
+      "Profile(s): \t{}\n----------------------------------------------------------",
+      env.getProperty("spring.application.name"),
+      protocol,
+      serverPort,
+      contextPath,
+      protocol,
+      hostAddress,
+      serverPort,
+      contextPath,
+      env.getActiveProfiles()
+    );
+  }
+}

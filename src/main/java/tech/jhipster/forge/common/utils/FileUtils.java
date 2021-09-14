b9beda1064d924b3bb209a637f529846932c058b
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-09-14 08:39:19 +0200

    Improve File Utils

--- src/main/java/tech/jhipster/forge/common/utils/FileUtils.java
+++ src/main/java/tech/jhipster/forge/common/utils/FileUtils.java
@@ -2,11 +2,13 @@ package tech.jhipster.forge.common.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.UUID;
+import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.jhipster.forge.error.domain.Assert;
@@ -34,31 +36,48 @@ public class FileUtils {
   }
 
   public static String tmpDirForTest() {
-    return getPath(tmpDir(), "jhforge-test-" + UUID.randomUUID());
+    return getPath(tmpDir(), "jhforge-test", UUID.randomUUID().toString());
   }
 
   public static String getPath(String... paths) {
-    return String.join(File.separator, paths);
+    return String.join(File.separator, paths).replaceAll("/", File.separator).replaceAll("\\\\", File.separator);
   }
 
   public static Path getPathOf(String... paths) {
     return Path.of(getPath(paths));
   }
 
-  public static boolean contains(String filename, String value) {
+  public static String read(String filename) throws IOException {
+    return Files.readString(getPathOf(filename), StandardCharsets.UTF_8);
+  }
+
+  public static int getLine(String filename, String value) throws IOException {
     File file = new File(filename);
-    boolean findValue = false;
+    int lineNumber = 0;
     try (Scanner scanner = new Scanner(file)) {
       while (scanner.hasNextLine()) {
+        lineNumber++;
         String line = scanner.nextLine();
         if (line.contains(value)) {
-          findValue = true;
-          break;
+          return lineNumber;
         }
       }
+    }
+    return -1;
+  }
+
+  public static boolean containsInLine(String filename, String value) {
+    boolean findValue = false;
+    try {
+      findValue = getLine(filename, value) != -1;
     } catch (IOException ex) {
       log.error("The file {} does not exist", filename);
     }
     return findValue;
+  }
+
+  public static String replace(String filename, String regexp, String replacement) throws IOException {
+    String text = read(filename);
+    return Pattern.compile(regexp).matcher(text).replaceAll(replacement);
   }
 }
--- src/test/java/tech/jhipster/forge/common/utils/FileUtilsTest.java
+++ src/test/java/tech/jhipster/forge/common/utils/FileUtilsTest.java
@@ -9,7 +9,9 @@ import static tech.jhipster.forge.common.utils.FileUtils.getPath;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import tech.jhipster.forge.UnitTest;
 import tech.jhipster.forge.error.domain.MissingMandatoryValueException;
@@ -17,95 +19,220 @@ import tech.jhipster.forge.error.domain.MissingMandatoryValueException;
 @UnitTest
 class FileUtilsTest {
 
-  @Test
-  void shouldNotExistsForNull() {
-    assertThatThrownBy(() -> FileUtils.exists(null)).isExactlyInstanceOf(MissingMandatoryValueException.class).hasMessageContaining("path");
+  @Nested
+  class Exists {
+
+    @Test
+    void shouldExists() {
+      String tmp = FileUtils.tmpDir();
+
+      assertTrue(FileUtils.exists(tmp));
+    }
+
+    @Test
+    void shouldNotExistsForNull() {
+      assertThatThrownBy(() -> FileUtils.exists(null))
+        .isExactlyInstanceOf(MissingMandatoryValueException.class)
+        .hasMessageContaining("path");
+    }
+
+    @Test
+    void shouldNotExistsForBlank() {
+      assertThatThrownBy(() -> FileUtils.exists(null))
+        .isExactlyInstanceOf(MissingMandatoryValueException.class)
+        .hasMessageContaining("path");
+    }
   }
 
-  @Test
-  void shouldNotExistsForBlank() {
-    assertThatThrownBy(() -> FileUtils.exists(null)).isExactlyInstanceOf(MissingMandatoryValueException.class).hasMessageContaining("path");
+  @Nested
+  class CreateFolder {
+
+    @Test
+    void shouldNotCreateFolderForNull() {
+      assertThatThrownBy(() -> FileUtils.createFolder(null))
+        .isExactlyInstanceOf(MissingMandatoryValueException.class)
+        .hasMessageContaining("path");
+    }
+
+    @Test
+    void shouldNotCreateFolderForBlank() {
+      assertThatThrownBy(() -> FileUtils.createFolder(null))
+        .isExactlyInstanceOf(MissingMandatoryValueException.class)
+        .hasMessageContaining("path");
+    }
+
+    @Test
+    void shouldCreateFolderOnlyOnceTime() {
+      String path = FileUtils.tmpDirForTest();
+
+      assertFileNotExist(path);
+
+      assertThatCode(() -> FileUtils.createFolder(path)).doesNotThrowAnyException();
+      assertTrue(FileUtils.exists(path));
+
+      assertThatCode(() -> FileUtils.createFolder(path)).doesNotThrowAnyException();
+      assertTrue(FileUtils.exists(path));
+    }
+
+    @Test
+    void shouldNotCreateFolderWhenItsAFile() throws Exception {
+      String path = FileUtils.tmpDirForTest();
+      String destinationFile = getPath(path, "chips");
+
+      assertFalse(FileUtils.exists(path));
+      assertThatCode(() -> FileUtils.createFolder(path)).doesNotThrowAnyException();
+      assertTrue(FileUtils.exists(path));
+      Files.createFile(FileUtils.getPathOf(destinationFile));
+
+      assertThatThrownBy(() -> FileUtils.createFolder(destinationFile)).isInstanceOf(IOException.class);
+    }
   }
 
-  @Test
-  void shouldExists() {
-    String tmp = FileUtils.tmpDir();
+  @Nested
+  class GetPath {
 
-    assertTrue(FileUtils.exists(tmp));
+    @Test
+    void shouldGetPath() {
+      String result = getPath("chips", "beer");
+
+      assertThat(result).isEqualTo("chips" + File.separator + "beer");
+    }
+
+    @Test
+    void shouldGetPathForLinux() {
+      String result = getPath("/home/chips/beer");
+
+      assertThat(result).isEqualTo(File.separator + "home" + File.separator + "chips" + File.separator + "beer");
+    }
+
+    @Test
+    void shouldGetPathForWindows() {
+      String result = getPath("C:\\chips\\beer");
+
+      assertThat(result).isEqualTo("C:" + File.separator + "chips" + File.separator + "beer");
+    }
+
+    @Test
+    void shouldGetPathOf() {
+      Path result = FileUtils.getPathOf("chips", "beer");
+
+      assertThat(result).isEqualTo(Path.of("chips" + File.separator + "beer"));
+    }
   }
 
-  @Test
-  void shouldNotCreateFolderForNull() {
-    assertThatThrownBy(() -> FileUtils.createFolder(null))
-      .isExactlyInstanceOf(MissingMandatoryValueException.class)
-      .hasMessageContaining("path");
+  @Nested
+  class Read {
+
+    @Test
+    void shouldRead() throws Exception {
+      String filename = getPath("src/test/resources/template/utils/readme-short.md");
+
+      String result = FileUtils.read(filename);
+
+      String lineSeparator = System.lineSeparator();
+      String expectedResult = new StringBuilder()
+        .append("this is a short readme")
+        .append(lineSeparator)
+        .append("used for unit tests")
+        .append(lineSeparator)
+        .append("powered by JHipster \uD83E\uDD13")
+        .append(lineSeparator)
+        .toString();
+      assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldNotReadWhenFileNotExist() {
+      String filename = getPath("src/test/resources/template/utils/unknown.md");
+
+      assertThatThrownBy(() -> FileUtils.read(filename)).isExactlyInstanceOf(NoSuchFileException.class);
+    }
   }
 
-  @Test
-  void shouldNotCreateFolderForBlank() {
-    assertThatThrownBy(() -> FileUtils.createFolder(null))
-      .isExactlyInstanceOf(MissingMandatoryValueException.class)
-      .hasMessageContaining("path");
+  @Nested
+  class GetLine {
+
+    @Test
+    void shouldGetLine() throws Exception {
+      String filename = getPath("src/test/resources/template/utils/readme-short.md");
+
+      assertThat(FileUtils.getLine(filename, "used for unit tests")).isEqualTo(2);
+      assertThat(FileUtils.getLine(filename, "JHipster")).isEqualTo(3);
+    }
+
+    @Test
+    void shouldNotGetLineAsCaseSensitive() throws Exception {
+      String filename = getPath("src/test/resources/template/utils/readme-short.md");
+
+      assertThat(FileUtils.getLine(filename, "jhipster")).isEqualTo(-1);
+    }
+
+    @Test
+    void shouldNotGetLineForAnotherText() throws Exception {
+      String filename = getPath("src/test/resources/template/utils/readme-short.md");
+
+      assertThat(FileUtils.getLine(filename, "beer")).isEqualTo(-1);
+    }
+
+    @Test
+    void shouldNotGetLineWhenFileNotExist() {
+      String filename = getPath("src/test/resources/template/utils/unknown.md");
+
+      assertThatThrownBy(() -> FileUtils.getLine(filename, "beer")).isInstanceOf(IOException.class);
+    }
   }
 
-  @Test
-  void shouldCreateFolderOnlyOnceTime() {
-    String path = FileUtils.tmpDirForTest();
+  @Nested
+  class ContainsInLine {
 
-    assertFileNotExist(path);
+    @Test
+    void shouldContainsInLine() {
+      String filename = getPath("src", "test", "resources", "template", "utils", "example-readme.md");
 
-    assertThatCode(() -> FileUtils.createFolder(path)).doesNotThrowAnyException();
-    assertTrue(FileUtils.exists(path));
+      assertTrue(FileUtils.containsInLine(filename, "Before you can build this project"));
+    }
 
-    assertThatCode(() -> FileUtils.createFolder(path)).doesNotThrowAnyException();
-    assertTrue(FileUtils.exists(path));
+    @Test
+    void shouldNotContainsInLine() {
+      String filename = getPath("src", "test", "resources", "template", "utils", "example-readme.md");
+
+      assertFalse(FileUtils.containsInLine(filename, "apero with beers"));
+    }
+
+    @Test
+    void shouldNotContainsInLineWhenFilenameNotExist() {
+      String filename = getPath("src", "test", "resources", "template", "utils", "unknown.md");
+
+      assertFalse(FileUtils.containsInLine(filename, "apero with beers"));
+    }
   }
 
-  @Test
-  void shouldNotCreateFolderWhenItsAFile() throws Exception {
-    String path = FileUtils.tmpDirForTest();
-    String destinationFile = getPath(path, "chips");
+  @Nested
+  class Replace {
 
-    assertFalse(FileUtils.exists(path));
-    assertThatCode(() -> FileUtils.createFolder(path)).doesNotThrowAnyException();
-    assertTrue(FileUtils.exists(path));
-    Files.createFile(FileUtils.getPathOf(destinationFile));
+    @Test
+    void shouldReplace() throws Exception {
+      String filename = getPath("src/test/resources/template/utils/readme-short.md");
 
-    assertThatThrownBy(() -> FileUtils.createFolder(destinationFile)).isInstanceOf(IOException.class);
-  }
+      String result = FileUtils.replace(filename, "powered by JHipster \uD83E\uDD13", "Hello JHipster Forge");
 
-  @Test
-  void shouldGetPath() {
-    String result = getPath("chips", "beer");
+      String lineSeparator = System.lineSeparator();
+      String expectedResult = new StringBuilder()
+        .append("this is a short readme")
+        .append(lineSeparator)
+        .append("used for unit tests")
+        .append(lineSeparator)
+        .append("Hello JHipster Forge")
+        .append(lineSeparator)
+        .toString();
+      assertThat(result).isEqualTo(expectedResult);
+    }
 
-    assertThat(result).isEqualTo("chips" + File.separator + "beer");
-  }
+    @Test
+    void shouldNotReplaceWhenFileNotExist() {
+      String filename = getPath("src/test/resources/template/utils/unknown.md");
 
-  @Test
-  void shouldGetPathOf() {
-    Path result = FileUtils.getPathOf("chips", "beer");
-
-    assertThat(result).isEqualTo(Path.of("chips" + File.separator + "beer"));
-  }
-
-  @Test
-  void shouldContains() throws Exception {
-    String filename = getPath("src", "test", "resources", "template", "utils", "example-readme.md");
-
-    assertTrue(FileUtils.contains(filename, "Before you can build this project"));
-  }
-
-  @Test
-  void shouldNotContains() throws Exception {
-    String filename = getPath("src", "test", "resources", "template", "utils", "example-readme.md");
-
-    assertFalse(FileUtils.contains(filename, "apero with beers"));
-  }
-
-  @Test
-  void shouldNotContainsWhenFilenameNotExist() {
-    String filename = getPath("src", "test", "resources", "template", "utils", "unknown.md");
-
-    assertFalse(FileUtils.contains(filename, "apero with beers"));
+      assertThatThrownBy(() -> FileUtils.replace(filename, "powered by JHipster", "Hello JHipster Forge")).isInstanceOf(IOException.class);
+    }
   }
 }

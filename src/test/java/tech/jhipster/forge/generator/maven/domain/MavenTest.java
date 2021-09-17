952c925ac0a7a9e12f71a07824ab3cca29ba191a
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-09-17 09:38:32 +0200

    Add method to check if the project is a Maven Project

--- src/test/java/tech/jhipster/forge/generator/maven/domain/MavenTest.java
+++ src/test/java/tech/jhipster/forge/generator/maven/domain/MavenTest.java
@@ -2,15 +2,43 @@ package tech.jhipster.forge.generator.maven.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static tech.jhipster.forge.TestUtils.tmpProject;
+import static tech.jhipster.forge.common.utils.FileUtils.getPath;
 
+import java.nio.file.Files;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import tech.jhipster.forge.UnitTest;
+import tech.jhipster.forge.common.domain.Project;
+import tech.jhipster.forge.common.utils.FileUtils;
 import tech.jhipster.forge.error.domain.MissingMandatoryValueException;
 import tech.jhipster.forge.error.domain.UnauthorizedValueException;
 
 @UnitTest
 public class MavenTest {
+
+  @Nested
+  class IsMavenProject {
+
+    @Test
+    void shouldNotBeMavenProject() {
+      Project project = tmpProject();
+
+      assertFalse(Maven.isMavenProject(project));
+    }
+
+    @Test
+    void shouldBeMavenProject() throws Exception {
+      Project project = tmpProject();
+      String destinationFile = getPath(project.getPath(), "pom.xml");
+      FileUtils.createFolder(project.getPath());
+      Files.createFile(FileUtils.getPathOf(destinationFile));
+
+      assertTrue(Maven.isMavenProject(project));
+    }
+  }
 
   @Nested
   class GetParent {

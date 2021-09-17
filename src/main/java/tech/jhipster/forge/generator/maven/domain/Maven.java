6461e9958ac418bd6043da308e09a205213a6987
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-09-17 11:26:43 +0200

    Clean maven domain

--- src/main/java/tech/jhipster/forge/generator/maven/domain/Maven.java
+++ src/main/java/tech/jhipster/forge/generator/maven/domain/Maven.java
@@ -1,12 +1,9 @@
 package tech.jhipster.forge.generator.maven.domain;
 
 import static tech.jhipster.forge.common.utils.FileUtils.getPath;
-import static tech.jhipster.forge.common.utils.WordUtils.DEFAULT_INDENTATION;
-import static tech.jhipster.forge.common.utils.WordUtils.indent;
 
 import tech.jhipster.forge.common.domain.Project;
 import tech.jhipster.forge.common.utils.FileUtils;
-import tech.jhipster.forge.error.domain.Assert;
 
 public class Maven {
 
@@ -14,44 +11,5 @@ public class Maven {
 
   public static boolean isMavenProject(Project project) {
     return FileUtils.exists(getPath(project.getPath(), "pom.xml"));
-  }
-
-  public static String getDependencies(String groupId, String artifactId) {
-    return getDependencies(groupId, artifactId, false, DEFAULT_INDENTATION);
-  }
-
-  public static String getDependencies(String groupId, String artifactId, int indentation) {
-    return getDependencies(groupId, artifactId, false, indentation);
-  }
-
-  public static String getDependencies(String groupId, String artifactId, boolean scopeTest) {
-    return getDependencies(groupId, artifactId, scopeTest, DEFAULT_INDENTATION);
-  }
-
-  public static String getDependencies(String groupId, String artifactId, boolean scopeTest, int indentation) {
-    Assert.notBlank("groupId", groupId);
-    Assert.notBlank("artifactId", artifactId);
-
-    StringBuilder result = new StringBuilder()
-      .append("<dependency>")
-      .append(System.lineSeparator())
-      .append(indent(3, indentation))
-      .append("<groupId>")
-      .append(groupId)
-      .append("</groupId>")
-      .append(System.lineSeparator())
-      .append(indent(3, indentation))
-      .append("<artifactId>")
-      .append(artifactId)
-      .append("</artifactId>")
-      .append(System.lineSeparator());
-
-    if (scopeTest) {
-      result.append(indent(3, indentation)).append("<scope>test</scope>").append(System.lineSeparator());
-    }
-
-    result.append(indent(2, indentation)).append("</dependency>").append(System.lineSeparator());
-
-    return result.toString();
   }
 }
--- src/test/java/tech/jhipster/forge/generator/maven/domain/MavenTest.java
+++ src/test/java/tech/jhipster/forge/generator/maven/domain/MavenTest.java
@@ -1,134 +1,33 @@
 package tech.jhipster.forge.generator.maven.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static tech.jhipster.forge.TestUtils.tmpProject;
 import static tech.jhipster.forge.common.utils.FileUtils.getPath;
 
 import java.nio.file.Files;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import tech.jhipster.forge.UnitTest;
 import tech.jhipster.forge.common.domain.Project;
 import tech.jhipster.forge.common.utils.FileUtils;
-import tech.jhipster.forge.error.domain.MissingMandatoryValueException;
-import tech.jhipster.forge.error.domain.UnauthorizedValueException;
 
 @UnitTest
 public class MavenTest {
 
-  @Nested
-  class IsMavenProject {
+  @Test
+  void shouldNotBeMavenProject() {
+    Project project = tmpProject();
 
-    @Test
-    void shouldNotBeMavenProject() {
-      Project project = tmpProject();
-
-      assertFalse(Maven.isMavenProject(project));
-    }
-
-    @Test
-    void shouldBeMavenProject() throws Exception {
-      Project project = tmpProject();
-      String destinationFile = getPath(project.getPath(), "pom.xml");
-      FileUtils.createFolder(project.getPath());
-      Files.createFile(FileUtils.getPathOf(destinationFile));
-
-      assertTrue(Maven.isMavenProject(project));
-    }
+    assertFalse(Maven.isMavenProject(project));
   }
 
-  @Nested
-  class GetDependencies {
+  @Test
+  void shouldBeMavenProject() throws Exception {
+    Project project = tmpProject();
+    String destinationFile = getPath(project.getPath(), "pom.xml");
+    FileUtils.createFolder(project.getPath());
+    Files.createFile(FileUtils.getPathOf(destinationFile));
 
-    @Test
-    void shouldGetDependencies() {
-      // @formatter:off
-      String expected =
-        "<dependency>" + System.lineSeparator() +
-        "      <groupId>org.springframework.boot</groupId>" + System.lineSeparator() +
-        "      <artifactId>spring-boot-starter</artifactId>" + System.lineSeparator() +
-        "    </dependency>" + System.lineSeparator();
-      // @formatter:on
-
-      String result = Maven.getDependencies("org.springframework.boot", "spring-boot-starter");
-
-      assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    void shouldGetDependenciesWithScopeTest() {
-      // @formatter:off
-      String expected =
-        "<dependency>" + System.lineSeparator() +
-        "      <groupId>org.springframework.boot</groupId>" + System.lineSeparator() +
-        "      <artifactId>spring-boot-starter-test</artifactId>" + System.lineSeparator() +
-        "      <scope>test</scope>" + System.lineSeparator() +
-        "    </dependency>" + System.lineSeparator();
-      // @formatter:on
-
-      String result = Maven.getDependencies("org.springframework.boot", "spring-boot-starter-test", true);
-
-      assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    void shouldGetDependenciesWith4Indentations() {
-      // @formatter:off
-      String expected =
-        "<dependency>" + System.lineSeparator() +
-        "            <groupId>org.springframework.boot</groupId>" + System.lineSeparator() +
-        "            <artifactId>spring-boot-starter</artifactId>" + System.lineSeparator() +
-        "        </dependency>" + System.lineSeparator();
-      // @formatter:on
-
-      String result = Maven.getDependencies("org.springframework.boot", "spring-boot-starter", 4);
-
-      assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    void shouldNotGetDependenciesWithNullGroupId() {
-      assertThatThrownBy(() -> Maven.getDependencies(null, "spring-boot-starter"))
-        .isExactlyInstanceOf(MissingMandatoryValueException.class)
-        .hasMessageContaining("groupId");
-    }
-
-    @Test
-    void shouldNotGetDependenciesWithBlankGroupId() {
-      assertThatThrownBy(() -> Maven.getDependencies(" ", "spring-boot-starter"))
-        .isExactlyInstanceOf(MissingMandatoryValueException.class)
-        .hasMessageContaining("groupId");
-    }
-
-    @Test
-    void shouldNotGetDependenciesWithNullArtifactId() {
-      assertThatThrownBy(() -> Maven.getDependencies("org.springframework.boot", null))
-        .isExactlyInstanceOf(MissingMandatoryValueException.class)
-        .hasMessageContaining("artifactId");
-    }
-
-    @Test
-    void shouldNotGetDependenciesWithBlankArtifactId() {
-      assertThatThrownBy(() -> Maven.getDependencies("org.springframework.boot", " "))
-        .isExactlyInstanceOf(MissingMandatoryValueException.class)
-        .hasMessageContaining("artifactId");
-    }
-
-    @Test
-    void shouldNotGetDependenciesWithNegativeIndentation() {
-      assertThatThrownBy(() -> Maven.getDependencies("org.springframework.boot", "spring-boot-starter-parent", -1))
-        .isExactlyInstanceOf(UnauthorizedValueException.class)
-        .hasMessageContaining("spaceNumber");
-    }
-
-    @Test
-    void shouldNotGetParentWithZeroIndentation() {
-      assertThatThrownBy(() -> Maven.getDependencies("org.springframework.boot", "spring-boot-starter-parent", 0))
-        .isExactlyInstanceOf(UnauthorizedValueException.class)
-        .hasMessageContaining("spaceNumber");
-    }
+    assertTrue(Maven.isMavenProject(project));
   }
 }

7cc70a282b2bf3749b36309a5b6cf7c9bf141d1e
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-09-23 23:40:12 +0200

    Add Spring Boot Mvc

--- src/test/java/tech/jhipster/forge/JhforgeAppIT.java
+++ src/test/java/tech/jhipster/forge/JhforgeAppIT.java
@@ -1,12 +1,8 @@
 package tech.jhipster.forge;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 @IntegrationTest
 class JhforgeAppIT {
@@ -14,26 +10,5 @@ class JhforgeAppIT {
   @Test
   void shouldMain() {
     assertThatCode(() -> JhforgeApp.main(new String[] {})).doesNotThrowAnyException();
-  }
-
-  @Test
-  void shouldMainWithServerSslKeyStore() {
-    System.setProperty("server.ssl.key-store", "https://localhost");
-    assertThatCode(() -> JhforgeApp.main(new String[] {})).doesNotThrowAnyException();
-  }
-
-  @Test
-  void shouldMainWithContextPath() {
-    System.setProperty("server.servlet.context-path", "/chips");
-    assertThatCode(() -> JhforgeApp.main(new String[] {})).doesNotThrowAnyException();
-  }
-
-  @Test
-  void shouldMainWithoutHostAddress() {
-    try (MockedStatic<InetAddress> inetAddress = Mockito.mockStatic(InetAddress.class)) {
-      inetAddress.when(InetAddress::getLocalHost).thenThrow(new UnknownHostException());
-
-      assertThatCode(() -> JhforgeApp.main(new String[] {})).doesNotThrowAnyException();
-    }
   }
 }

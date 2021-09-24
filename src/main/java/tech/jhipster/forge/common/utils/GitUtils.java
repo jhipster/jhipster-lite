06e32cfb769c8588f0a92cb70ab9899e0cd4d701
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-09-24 09:59:51 +0200

    Improve GitUtils

--- src/main/java/tech/jhipster/forge/common/utils/GitUtils.java
+++ src/main/java/tech/jhipster/forge/common/utils/GitUtils.java
@@ -18,23 +18,35 @@ public class GitUtils {
   private GitUtils() {}
 
   public static Git init(String dir) throws GitAPIException {
-    log.info("Create Git repository for {}", dir);
+    log.info("Create Git repository: {}", dir);
     return Git.init().setDirectory(new File(dir)).call();
   }
 
-  public static void add(Git git, String dir) throws GitAPIException {
+  public static void addAndCommit(String dir, String message) throws GitAPIException, IOException {
+    add(dir);
+    commit(dir, message);
+  }
+
+  public static void add(String dir) throws GitAPIException, IOException {
     log.debug("Adding all files to repository {}", dir);
+    Git git = getGit(dir);
     git.add().addFilepattern(".").call();
   }
 
-  public static void commit(Git git, String dir, String message) throws GitAPIException {
+  public static void commit(String dir, String message) throws GitAPIException, IOException {
     log.debug("Commiting all files to repository {}", dir);
+    Git git = getGit(dir);
     git.commit().setCommitter(USERNAME, EMAIL).setMessage(message).call();
   }
 
-  public static void apply(Git git, String dir) throws GitAPIException, IOException {
-    log.debug("Apply patch {}", dir);
-    FileInputStream fileInputStream = new FileInputStream(new File(dir));
+  public static void apply(String dir, String patch) throws GitAPIException, IOException {
+    log.debug("Apply patch to repository {}: {}", dir, patch);
+    Git git = getGit(dir);
+    FileInputStream fileInputStream = new FileInputStream(patch);
     git.apply().setPatch(fileInputStream).call();
+  }
+
+  public static Git getGit(String dir) throws IOException {
+    return Git.open(new File(dir));
   }
 }

941b9f1c140c044bec7b1d3b77c4a7df020d6ffa
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-10-08 12:10:12 +0200

    Generator: add database postgresql

---
+++ src/main/java/tech/jhipster/forge/generator/springboot/application/PostgreSQLApplicationService.java
@@ -0,0 +1,47 @@
+package tech.jhipster.forge.generator.springboot.application;
+
+import org.springframework.stereotype.Service;
+import tech.jhipster.forge.common.domain.Project;
+import tech.jhipster.forge.generator.springboot.domain.usecase.PostgreSQLService;
+
+@Service
+public class PostgreSQLApplicationService {
+
+  private final PostgreSQLService postgreSQLService;
+
+  public PostgreSQLApplicationService(PostgreSQLService postgreSQLService) {
+    this.postgreSQLService = postgreSQLService;
+  }
+
+  public void addSpringDataJpa(Project project) {
+    postgreSQLService.addSpringDataJpa(project);
+  }
+
+  public void addPostgreSQLDriver(Project project) {
+    postgreSQLService.addPostgreSQLDriver(project);
+  }
+
+  public void addHikari(Project project) {
+    postgreSQLService.addHikari(project);
+  }
+
+  public void addHibernateCore(Project project) {
+    postgreSQLService.addHibernateCore(project);
+  }
+
+  public void addDockerCompose(Project project) {
+    postgreSQLService.addDockerCompose(project);
+  }
+
+  public void addDialectJava(Project project) {
+    postgreSQLService.addDialectJava(project);
+  }
+
+  public void addProperties(Project project) {
+    postgreSQLService.addProperties(project);
+  }
+
+  public void init(Project project) {
+    postgreSQLService.init(project);
+  }
+}
---
+++ src/main/java/tech/jhipster/forge/generator/springboot/domain/usecase/PostgreSQLService.java
@@ -0,0 +1,17 @@
+package tech.jhipster.forge.generator.springboot.domain.usecase;
+
+import tech.jhipster.forge.common.domain.Project;
+
+public interface PostgreSQLService {
+  void init(Project project);
+
+  void addSpringDataJpa(Project project);
+  void addPostgreSQLDriver(Project project);
+  void addHikari(Project project);
+  void addHibernateCore(Project project);
+
+  void addDockerCompose(Project project);
+  void addDialectJava(Project project);
+
+  void addProperties(Project project);
+}

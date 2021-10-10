8e0691ff748fecb28c226ed9a59f4c2b2550f504
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-10-10 16:40:49 +0200

    Generator: add liquibase

---
+++ src/main/java/tech/jhipster/forge/generator/springboot/domain/service/LiquibaseDomainService.java
@@ -0,0 +1,63 @@
+package tech.jhipster.forge.generator.springboot.domain.service;
+
+import static tech.jhipster.forge.common.domain.Constants.*;
+import static tech.jhipster.forge.common.utils.FileUtils.getPath;
+
+import tech.jhipster.forge.common.domain.Project;
+import tech.jhipster.forge.common.domain.ProjectRepository;
+import tech.jhipster.forge.generator.springboot.domain.model.Dependency;
+import tech.jhipster.forge.generator.springboot.domain.usecase.LiquibaseService;
+import tech.jhipster.forge.generator.springboot.domain.usecase.MavenService;
+import tech.jhipster.forge.generator.springboot.domain.usecase.SpringBootService;
+
+public class LiquibaseDomainService implements LiquibaseService {
+
+  public static final String SOURCE = "liquibase";
+  public static final String LIQUIBASE_PATH = "technical/secondary/liquibase";
+
+  public final ProjectRepository projectRepository;
+  public final MavenService mavenService;
+  public final SpringBootService springBootService;
+
+  public LiquibaseDomainService(ProjectRepository projectRepository, MavenService mavenService, SpringBootService springBootService) {
+    this.projectRepository = projectRepository;
+    this.mavenService = mavenService;
+    this.springBootService = springBootService;
+  }
+
+  @Override
+  public void init(Project project) {
+    addLiquibase(project);
+    addChangelogMasterXml(project);
+    addConfigurationJava(project);
+  }
+
+  @Override
+  public void addLiquibase(Project project) {
+    Dependency dependency = Dependency.builder().groupId("org.liquibase").artifactId("liquibase-core").build();
+    mavenService.addDependency(project, dependency);
+  }
+
+  @Override
+  public void addChangelogMasterXml(Project project) {
+    projectRepository.add(project, getPath(SOURCE, "resources"), "master.xml", getPath(MAIN_RESOURCES, "config/liquibase"));
+  }
+
+  @Override
+  public void addConfigurationJava(Project project) {
+    String packageNamePath = project.getPackageNamePath().orElse(getPath("com/mycompany/myapp"));
+
+    templateToLiquibase(project, packageNamePath, "src", "AsyncSpringLiquibase.java", MAIN_JAVA);
+    templateToLiquibase(project, packageNamePath, "src", "LiquibaseConfiguration.java", MAIN_JAVA);
+    templateToLiquibase(project, packageNamePath, "src", "SpringLiquibaseUtil.java", MAIN_JAVA);
+
+    templateToLiquibase(project, packageNamePath, "test", "AsyncSpringLiquibaseTest.java", TEST_JAVA);
+    templateToLiquibase(project, packageNamePath, "test", "LiquibaseConfigurationIT.java", TEST_JAVA);
+    templateToLiquibase(project, packageNamePath, "test", "LogbackRecorder.java", TEST_JAVA);
+    templateToLiquibase(project, packageNamePath, "test", "SpringLiquibaseUtilTest.java", TEST_JAVA);
+  }
+
+  private void templateToLiquibase(Project project, String source, String type, String sourceFilename, String destination) {
+    projectRepository.template(project, getPath(SOURCE, type), sourceFilename, getPath(destination, source, LIQUIBASE_PATH));
+  }
+}
---
+++ src/test/java/tech/jhipster/forge/generator/springboot/application/LiquibaseAssertFiles.java
@@ -0,0 +1,27 @@
+package tech.jhipster.forge.generator.springboot.application;
+
+import static tech.jhipster.forge.TestUtils.assertFileExist;
+import static tech.jhipster.forge.common.domain.Constants.*;
+import static tech.jhipster.forge.common.utils.FileUtils.getPath;
+
+import tech.jhipster.forge.common.domain.Project;
+
+public class LiquibaseAssertFiles {
+
+  public static void assertFilesLiquibaseJava(Project project) {
+    String liquibasePath = getPath(MAIN_JAVA, "com/mycompany/myapp/technical/secondary/liquibase");
+    assertFileExist(project, getPath(liquibasePath, "AsyncSpringLiquibase.java"));
+    assertFileExist(project, getPath(liquibasePath, "LiquibaseConfiguration.java"));
+    assertFileExist(project, getPath(liquibasePath, "SpringLiquibaseUtil.java"));
+
+    String liquibaseTestPath = getPath(TEST_JAVA, "com/mycompany/myapp/technical/secondary/liquibase");
+    assertFileExist(project, getPath(liquibaseTestPath, "AsyncSpringLiquibaseTest.java"));
+    assertFileExist(project, getPath(liquibaseTestPath, "LiquibaseConfigurationIT.java"));
+    assertFileExist(project, getPath(liquibaseTestPath, "LogbackRecorder.java"));
+    assertFileExist(project, getPath(liquibaseTestPath, "SpringLiquibaseUtilTest.java"));
+  }
+
+  public static void assertFilesLiquibaseChangelogMasterXml(Project project) {
+    assertFileExist(project, getPath(MAIN_RESOURCES, "config/liquibase/master.xml"));
+  }
+}

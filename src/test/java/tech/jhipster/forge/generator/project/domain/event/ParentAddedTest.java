451e468360d137220b8a2eed5d0b81a795b251cf
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-11-05 16:16:12 +0100

    Project: add ParentAdded

---
+++ src/test/java/tech/jhipster/forge/generator/project/domain/event/ParentAddedTest.java
@@ -0,0 +1,43 @@
+package tech.jhipster.forge.generator.project.domain.event;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static tech.jhipster.forge.TestUtils.tmpProject;
+
+import org.junit.jupiter.api.Test;
+import tech.jhipster.forge.UnitTest;
+import tech.jhipster.forge.error.domain.MissingMandatoryValueException;
+import tech.jhipster.forge.generator.project.domain.Parent;
+import tech.jhipster.forge.generator.project.domain.Project;
+
+@UnitTest
+class ParentAddedTest {
+
+  @Test
+  void shouldBuild() {
+    Project project = tmpProject();
+    Parent parent = getParent();
+    ParentAdded parentAdded = ParentAdded.of(project, parent);
+
+    assertThat(parentAdded.project()).usingRecursiveComparison().isEqualTo(project);
+    assertThat(parentAdded.parent()).usingRecursiveComparison().isEqualTo(parent);
+  }
+
+  @Test
+  void shouldNotBuildWithNullProject() {
+    assertThatThrownBy(() -> ParentAdded.of(null, getParent()))
+      .isExactlyInstanceOf(MissingMandatoryValueException.class)
+      .hasMessageContaining("project");
+  }
+
+  @Test
+  void shouldNotBuildWithNullParent() {
+    assertThatThrownBy(() -> ParentAdded.of(tmpProject(), null))
+      .isExactlyInstanceOf(MissingMandatoryValueException.class)
+      .hasMessageContaining("parent");
+  }
+
+  private Parent getParent() {
+    return Parent.builder().groupId("org.springframework.boot").artifactId("spring-boot-starter-parent").version("2.5.3").build();
+  }
+}

012bc99db49c5e5f19f282d2b0502d2b62e005a2
Swaraj Pal <swarajpalsaaj@yahoo.co.in>
2021-12-14 01:48:50 +0530

    Remove unwanted import

--- src/test/java/tech/jhipster/lite/generator/server/springboot/database/mysql/domain/MySQLTest.java
+++ src/test/java/tech/jhipster/lite/generator/server/springboot/database/mysql/domain/MySQLTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import tech.jhipster.lite.UnitTest;
-import tech.jhipster.lite.generator.server.springboot.database.postgresql.domain.Postgresql;
 
 @UnitTest
 class MySQLTest {

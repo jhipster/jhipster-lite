6d012eda9991a66eae3794a1002b40091f382533
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-12-19 10:24:11 +0100

    Fix Code smell: String literals should not be duplicated

--- src/main/java/tech/jhipster/lite/generator/buildtool/maven/domain/MavenDomainService.java
+++ src/main/java/tech/jhipster/lite/generator/buildtool/maven/domain/MavenDomainService.java
@@ -128,7 +128,7 @@ public class MavenDomainService implements MavenService {
     String baseName = project.getBaseName().orElse("");
     project.addConfig("dasherizedBaseName", WordUtils.kebabCase(baseName));
 
-    projectRepository.template(project, SOURCE, "pom.xml");
+    projectRepository.template(project, SOURCE, POM_XML);
   }
 
   @Override

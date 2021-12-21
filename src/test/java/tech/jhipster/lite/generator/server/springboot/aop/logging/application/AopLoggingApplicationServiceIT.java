c5645869e3b887deecc20cdb79c138791100264e
Marc SAGUER <saguermarc@gmail.com>
2021-12-21 15:15:20 +0000

    Add aop logging tests

---
+++ src/test/java/tech/jhipster/lite/generator/server/springboot/aop/logging/application/AopLoggingApplicationServiceIT.java
@@ -0,0 +1,139 @@
+package tech.jhipster.lite.generator.server.springboot.aop.logging.application;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static tech.jhipster.lite.TestUtils.*;
+import static tech.jhipster.lite.common.domain.FileUtils.getPath;
+import static tech.jhipster.lite.generator.buildtool.maven.domain.MavenDomainService.POM_XML;
+import static tech.jhipster.lite.generator.project.domain.Constants.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.jhipster.lite.IntegrationTest;
+import tech.jhipster.lite.error.domain.GeneratorException;
+import tech.jhipster.lite.generator.buildtool.maven.application.MavenApplicationService;
+import tech.jhipster.lite.generator.init.application.InitApplicationService;
+import tech.jhipster.lite.generator.project.domain.Project;
+import tech.jhipster.lite.generator.server.springboot.core.application.SpringBootApplicationService;
+
+@IntegrationTest
+class AopLoggingApplicationServiceIT {
+
+  @Autowired
+  InitApplicationService initApplicationService;
+
+  @Autowired
+  MavenApplicationService mavenApplicationService;
+
+  @Autowired
+  SpringBootApplicationService springBootApplicationService;
+
+  @Autowired
+  AopLoggingApplicationService aopLoggingApplicationService;
+
+  @Test
+  void shouldInit() {
+    Project project = tmpProject();
+    project.addConfig("springBootVersion", "2.5.3");
+    initApplicationService.init(project);
+    mavenApplicationService.addPomXml(project);
+    springBootApplicationService.init(project);
+
+    aopLoggingApplicationService.init(project);
+
+    assertFileContent(
+      project,
+      POM_XML,
+      List.of("<groupId>org.springframework.boot</groupId>", "<artifactId>spring-boot-starter-aop</artifactId>")
+    );
+
+    assertFileExist(project, getPath(MAIN_JAVA, "com/mycompany/myapp/technical/infrastructure/secondary/aop/logging/LoggingAspect.java"));
+    assertFileExist(
+      project,
+      getPath(MAIN_JAVA, "com/mycompany/myapp/technical/infrastructure/secondary/aop/logging/LoggingAspectConfiguration.java")
+    );
+
+    assertFileContent(project, getPath(MAIN_RESOURCES, "config/application.properties"), "spring.aop.logging=false");
+    assertFileContent(project, getPath(MAIN_RESOURCES, "config/application-fast.properties"), "spring.aop.logging=true");
+    assertFileContent(project, getPath(TEST_RESOURCES, "config/application.properties"), "spring.aop.logging=true");
+  }
+
+  @Test
+  void shouldAddDialectJava() {
+    Project project = tmpProject();
+    project.addConfig("springBootVersion", "2.5.3");
+    initApplicationService.init(project);
+    mavenApplicationService.addPomXml(project);
+    springBootApplicationService.init(project);
+
+    aopLoggingApplicationService.addDialectJava(project);
+
+    assertFileExist(project, getPath(MAIN_JAVA, "com/mycompany/myapp/technical/infrastructure/secondary/aop/logging/LoggingAspect.java"));
+    assertFileExist(
+      project,
+      getPath(MAIN_JAVA, "com/mycompany/myapp/technical/infrastructure/secondary/aop/logging/LoggingAspectConfiguration.java")
+    );
+  }
+
+  @Test
+  void shouldMavenDependencies() {
+    Project project = tmpProject();
+    project.addConfig("springBootVersion", "2.5.3");
+    initApplicationService.init(project);
+    mavenApplicationService.addPomXml(project);
+
+    aopLoggingApplicationService.addMavenDependencies(project);
+
+    assertFileContent(
+      project,
+      POM_XML,
+      List.of("<groupId>org.springframework.boot</groupId>", "<artifactId>spring-boot-starter-aop</artifactId>")
+    );
+  }
+
+  @Test
+  void shouldNotAddMavenDendenciesWhenNoPomXml() {
+    Project project = tmpProject();
+
+    assertThatThrownBy(() -> aopLoggingApplicationService.addMavenDependencies(project)).isExactlyInstanceOf(GeneratorException.class);
+  }
+
+  @Test
+  void shoulAddProperties() {
+    Project project = tmpProject();
+    project.addConfig("springBootVersion", "2.5.3");
+    initApplicationService.init(project);
+    mavenApplicationService.addPomXml(project);
+    springBootApplicationService.init(project);
+
+    aopLoggingApplicationService.addProperties(project);
+
+    assertFileContent(project, getPath(MAIN_RESOURCES, "config/application.properties"), "spring.aop.logging=false");
+    assertFileContent(project, getPath(MAIN_RESOURCES, "config/application-fast.properties"), "spring.aop.logging=true");
+    assertFileContent(project, getPath(TEST_RESOURCES, "config/application.properties"), "spring.aop.logging=true");
+  }
+
+  @Test
+  void shouldNotAddPropertiesWhenNoSpringbootInit() {
+    Project project = tmpProject();
+    initApplicationService.init(project);
+
+    assertThatThrownBy(() -> aopLoggingApplicationService.addProperties(project)).isExactlyInstanceOf(GeneratorException.class);
+  }
+
+  @Test
+  void shouldAddJavaClasses() {
+    Project project = tmpProject();
+    initApplicationService.init(project);
+    mavenApplicationService.addPomXml(project);
+    springBootApplicationService.addMainApp(project);
+
+    aopLoggingApplicationService.addDialectJava(project);
+
+    assertFileExist(project, getPath(MAIN_JAVA, "com/mycompany/myapp/technical/infrastructure/secondary/aop/logging/LoggingAspect.java"));
+    assertFileExist(
+      project,
+      getPath(MAIN_JAVA, "com/mycompany/myapp/technical/infrastructure/secondary/aop/logging/LoggingAspectConfiguration.java")
+    );
+  }
+}

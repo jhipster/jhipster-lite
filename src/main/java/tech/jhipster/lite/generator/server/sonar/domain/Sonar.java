1b433501bb7f44f6045ce56df703eca9c6f0c990
Swaraj Pal <swarajpalsaaj@yahoo.co.in>
2021-12-22 00:59:16 +0530

    Sonar: Add priv constructor

--- src/main/java/tech/jhipster/lite/generator/server/sonar/domain/Sonar.java
+++ src/main/java/tech/jhipster/lite/generator/server/sonar/domain/Sonar.java
@@ -5,6 +5,8 @@ public class Sonar {
   public static final String SONARSOURCE_MAVEN_PLUGIN_VERSION = "3.9.1.2184";
   public static final String SONARQUBE_DOCKER_VERSION = "9.2.4-community";
 
+  private Sonar() {}
+
   public static String getMavenPluginVersion() {
     return SONARSOURCE_MAVEN_PLUGIN_VERSION;
   }

2fb19d8051e787810974164214821370ee25c27b
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-01-02 22:56:16 +0100

    Package Manager NPM: add method for adding dependency

--- src/main/java/tech/jhipster/lite/generator/packagemanager/npm/domain/NpmDomainService.java
+++ src/main/java/tech/jhipster/lite/generator/packagemanager/npm/domain/NpmDomainService.java
@@ -1,19 +1,40 @@
 package tech.jhipster.lite.generator.packagemanager.npm.domain;
 
+import static tech.jhipster.lite.common.domain.WordUtils.indent;
+import static tech.jhipster.lite.generator.project.domain.DefaultConfig.*;
+
 import tech.jhipster.lite.generator.project.domain.CommandRepository;
 import tech.jhipster.lite.generator.project.domain.Project;
+import tech.jhipster.lite.generator.project.domain.ProjectRepository;
 
 public class NpmDomainService implements NpmService {
 
   private final CommandRepository commandRepository;
+  private final ProjectRepository projectRepository;
 
-  public NpmDomainService(CommandRepository commandRepository) {
+  public NpmDomainService(CommandRepository commandRepository, ProjectRepository projectRepository) {
     this.commandRepository = commandRepository;
+    this.projectRepository = projectRepository;
   }
 
   @Override
   public void addDependency(Project project, String dependency, String version) {
-    // Need to be implemented
+    project.addDefaultConfig(PRETTIER_DEFAULT_INDENT);
+    int indent = (Integer) project.getConfig(PRETTIER_DEFAULT_INDENT).orElse(2);
+
+    String needle = "\"dependencies\": \\{";
+    String newText = needle + System.lineSeparator() + indent(2, indent) + "\"" + dependency + "\": \"" + version + "\"";
+
+    String devDependenciesNeedle = "\"devDependencies\": \\{";
+    if (!projectRepository.containsRegexp(project, "", "package.json", needle)) {
+      newText =
+        newText + System.lineSeparator() + indent(1, indent) + "\\}," + System.lineSeparator() + indent(1, indent) + devDependenciesNeedle;
+      projectRepository.replaceText(project, "", "package.json", devDependenciesNeedle, newText);
+    } else if (projectRepository.containsRegexp(project, "", "package.json", needle + "\\}")) {
+      projectRepository.replaceText(project, "", "package.json", needle, newText + System.lineSeparator() + indent(1, indent));
+    } else {
+      projectRepository.replaceText(project, "", "package.json", needle, newText + ",");
+    }
   }
 
   @Override
--- src/test/java/tech/jhipster/lite/generator/packagemanager/npm/application/NpmApplicationServiceIT.java
+++ src/test/java/tech/jhipster/lite/generator/packagemanager/npm/application/NpmApplicationServiceIT.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static tech.jhipster.lite.TestUtils.*;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.SpyBean;
@@ -19,6 +20,40 @@ class NpmApplicationServiceIT {
 
   @SpyBean
   CommandRepository commandRepository;
+
+  @Test
+  void shouldAddDependencyWhenNoDependencyEntry() {
+    Project project = tmpProjectWithPackageJsonNoDependencies();
+    String dependency = "husky";
+    String version = "7.0.4";
+
+    npmApplicationService.addDependency(project, dependency, version);
+
+    assertFileContent(project, "package.json", List.of("\"dependencies\": {", "\"husky\": \"7.0.4\"", "},", "\"devDependencies\": {"));
+  }
+
+  @Test
+  void shouldAddDependencyWhenDependencyEmpty() {
+    Project project = tmpProjectWithPackageJsonEmpty();
+    String dependency = "husky";
+    String version = "7.0.4";
+
+    npmApplicationService.addDependency(project, dependency, version);
+
+    assertFileContent(project, "package.json", List.of("\"dependencies\": {", "\"husky\": \"7.0.4\""));
+    assertFileNoContent(project, "package.json", List.of("\"dependencies\": {", "\"husky\": \"7.0.4\","));
+  }
+
+  @Test
+  void shouldAddDependency() {
+    Project project = tmpProjectWithPackageJsonComplete();
+    String dependency = "husky";
+    String version = "7.0.4";
+
+    npmApplicationService.addDependency(project, dependency, version);
+
+    assertFileContent(project, "package.json", List.of("\"dependencies\": {", "\"husky\": \"7.0.4\","));
+  }
 
   @Test
   void shouldNpmInstall() {

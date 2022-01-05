3e220829a2e681b8d7f0972f399d44e581f394a6
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-01-05 22:46:21 +0100

    Refactoring to avoid duplicate code

--- src/main/java/tech/jhipster/lite/generator/packagemanager/npm/domain/NpmDomainService.java
+++ src/main/java/tech/jhipster/lite/generator/packagemanager/npm/domain/NpmDomainService.java
@@ -25,51 +25,25 @@ public class NpmDomainService implements NpmService {
 
   @Override
   public void addDependency(Project project, String dependency, String version) {
-    project.addDefaultConfig(PRETTIER_DEFAULT_INDENT);
-    int indent = (Integer) project.getConfig(PRETTIER_DEFAULT_INDENT).orElse(2);
-
-    String needle = DEPENDENCIES + ": " + OB;
-    String newText = needle + System.lineSeparator() + indent(2, indent) + DQ + dependency + DQ + ": " + DQ + version + DQ;
-
-    String versionNeedle = VERSION;
-    if (!projectRepository.containsRegexp(project, "", PACKAGE_JSON, needle)) {
-      newText =
-        newText + System.lineSeparator() + indent(1, indent) + CB + "," + System.lineSeparator() + indent(1, indent) + versionNeedle;
-      projectRepository.replaceText(project, "", PACKAGE_JSON, versionNeedle, newText);
-    } else if (projectRepository.containsRegexp(project, "", PACKAGE_JSON, needle + CB)) {
-      projectRepository.replaceText(project, "", PACKAGE_JSON, needle, newText + System.lineSeparator() + indent(1, indent));
-    } else {
-      projectRepository.replaceText(project, "", PACKAGE_JSON, needle, newText + ",");
-    }
+    addInformationToPackageJson(project, dependency, version, DEPENDENCIES);
   }
 
   @Override
   public void addDevDependency(Project project, String dependency, String version) {
-    project.addDefaultConfig(PRETTIER_DEFAULT_INDENT);
-    int indent = (Integer) project.getConfig(PRETTIER_DEFAULT_INDENT).orElse(2);
-
-    String needle = DEV_DEPENDENCIES + ": " + OB;
-    String newText = needle + System.lineSeparator() + indent(2, indent) + DQ + dependency + DQ + ": " + DQ + version + DQ;
-
-    String versionNeedle = VERSION;
-    if (!projectRepository.containsRegexp(project, "", PACKAGE_JSON, needle)) {
-      newText =
-        newText + System.lineSeparator() + indent(1, indent) + CB + "," + System.lineSeparator() + indent(1, indent) + versionNeedle;
-      projectRepository.replaceText(project, "", PACKAGE_JSON, versionNeedle, newText);
-    } else if (projectRepository.containsRegexp(project, "", PACKAGE_JSON, needle + CB)) {
-      projectRepository.replaceText(project, "", PACKAGE_JSON, needle, newText + System.lineSeparator() + indent(1, indent));
-    } else {
-      projectRepository.replaceText(project, "", PACKAGE_JSON, needle, newText + ",");
-    }
+    addInformationToPackageJson(project, dependency, version, DEV_DEPENDENCIES);
   }
 
   @Override
   public void addScript(Project project, String name, String cmd) {
+    addInformationToPackageJson(project, name, cmd, SCRIPTS);
+  }
+
+  private void addInformationToPackageJson(Project project, String dependency, String version, String key) {
     project.addDefaultConfig(PRETTIER_DEFAULT_INDENT);
     int indent = (Integer) project.getConfig(PRETTIER_DEFAULT_INDENT).orElse(2);
 
-    String needle = SCRIPTS + ": " + OB;
-    String newText = needle + System.lineSeparator() + indent(2, indent) + DQ + name + DQ + ": " + DQ + cmd + DQ;
+    String needle = key + ": " + OB;
+    String newText = needle + System.lineSeparator() + indent(2, indent) + DQ + dependency + DQ + ": " + DQ + version + DQ;
 
     String versionNeedle = VERSION;
     if (!projectRepository.containsRegexp(project, "", PACKAGE_JSON, needle)) {

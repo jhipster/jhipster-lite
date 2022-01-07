81c806fc68d325411a4ad86fde55d0009daca6fb
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-01-07 10:44:00 +0100

    Typescript: refactoring to list dependencies, scripts and files

--- src/main/java/tech/jhipster/lite/generator/typescript/domain/TypescriptDomainService.java
+++ src/main/java/tech/jhipster/lite/generator/typescript/domain/TypescriptDomainService.java
@@ -18,25 +18,8 @@ public class TypescriptDomainService implements TypescriptService {
 
   @Override
   public void init(Project project) {
-    npmService.addDependency(project, "@types/jest", "^26.0.22");
-    npmService.addDependency(project, "@typescript-eslint/eslint-plugin", "^4.21.0");
-    npmService.addDependency(project, "@typescript-eslint/parser", "^4.21.0");
-    npmService.addDependency(project, "eslint", "^7.23.0");
-    npmService.addDependency(project, "eslint-import-resolver-typescript", "^2.4.0");
-    npmService.addDependency(project, "eslint-plugin-import", "^2.22.1");
-    npmService.addDependency(project, "eslint-plugin-prettier", "^3.3.1");
-    npmService.addDependency(project, "jest", "^26.6.3");
-    npmService.addDependency(project, "ts-jest", "^26.5.4");
-    npmService.addDependency(project, "typescript", "^4.2.4");
-
-    npmService.addScript(project, "test", "jest");
-    npmService.addScript(project, "test:watch", "jest --watch");
-    npmService.addScript(project, "test:watch:all", "jest --watchAll");
-    npmService.addScript(project, "eslint:ci", "eslint './**/*.{ts,js}'");
-    npmService.addScript(project, "eslint", "eslint './**/*.{ts,js}' --fix");
-
-    projectRepository.add(project, SOURCE, ".eslintrc.js");
-    projectRepository.add(project, SOURCE, "jest.config.js");
-    projectRepository.add(project, SOURCE, "tsconfig.json");
+    Typescript.dependencies().forEach((dependency, version) -> npmService.addDependency(project, dependency, version));
+    Typescript.scripts().forEach((name, cmd) -> npmService.addScript(project, name, cmd));
+    Typescript.files().forEach(file -> projectRepository.add(project, SOURCE, file));
   }
 }

eb9159ab36545f97b85ebdd42d49d1c33743eb40
Daniel Franco <dandrfranco@gmail.com>
2022-01-11 01:07:56 +0000

    Add addRepository to BuildToolDomainService class

--- src/main/java/tech/jhipster/lite/generator/buildtool/generic/domain/BuildToolDomainService.java
+++ src/main/java/tech/jhipster/lite/generator/buildtool/generic/domain/BuildToolDomainService.java
@@ -81,6 +81,15 @@ public class BuildToolDomainService implements BuildToolService {
   }
 
   @Override
+  public void addRepository(Project project, Repository repository) {
+    if (project.isMavenProject()) {
+      mavenService.addRepository(project, repository);
+    } else {
+      throw new GeneratorException(EXCEPTION_NO_BUILD_TOOL);
+    }
+  }
+
+  @Override
   public void init(Project project, BuildToolType buildTool) {
     if (buildTool == MAVEN) {
       mavenService.initJava(project);

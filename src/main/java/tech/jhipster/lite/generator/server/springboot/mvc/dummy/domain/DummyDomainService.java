56ff1456ffbd7ef458b1d55acd39afef0a559d3d
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-01-13 22:10:06 +0100

    Dummy: add tests for Dummy Domain Service

---
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/mvc/dummy/domain/DummyDomainService.java
@@ -0,0 +1,12 @@
+package tech.jhipster.lite.generator.server.springboot.mvc.dummy.domain;
+
+import tech.jhipster.lite.generator.project.domain.Project;
+
+public class DummyDomainService implements DummyService {
+
+  @Override
+  public void applyDummyGitPatch(Project project) {
+    // TODO Auto-generated method stub
+
+  }
+}

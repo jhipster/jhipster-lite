3c3fcd6331c3164bf4f54d4c79f5e168d32b6be3
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-01-13 22:14:47 +0100

    Dummy: implement Dummy Domain Service

--- src/main/java/tech/jhipster/lite/generator/server/springboot/mvc/dummy/domain/DummyDomainService.java
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/mvc/dummy/domain/DummyDomainService.java
@@ -1,12 +1,24 @@
 package tech.jhipster.lite.generator.server.springboot.mvc.dummy.domain;
 
+import tech.jhipster.lite.common.domain.FileUtils;
 import tech.jhipster.lite.generator.project.domain.Project;
+import tech.jhipster.lite.generator.project.domain.ProjectRepository;
 
 public class DummyDomainService implements DummyService {
 
+  public static final String SOURCE = "server/springboot/mvc/dummy";
+  public static final String PATCH = "dummy.patch";
+
+  private final ProjectRepository projectRepository;
+
+  public DummyDomainService(ProjectRepository projectRepository) {
+    this.projectRepository = projectRepository;
+  }
+
   @Override
   public void applyDummyGitPatch(Project project) {
-    // TODO Auto-generated method stub
-
+    projectRepository.gitInit(project);
+    projectRepository.add(project, SOURCE, PATCH, ".jhipster");
+    projectRepository.gitApplyPatch(project, FileUtils.getPath(project.getFolder(), ".jhipster", PATCH));
   }
 }

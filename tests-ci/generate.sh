0f83ef49f13690c6d8c22b8b13ecbd02dd738df8
Bolo <bolo.sall89@gmail.com>
2022-01-24 19:06:50 +0100

    Add Flyway / in generate.sh

--- tests-ci/generate.sh
+++ tests-ci/generate.sh
@@ -53,9 +53,9 @@ elif [[ $filename == 'tomcat-mysql-ehcachexml' ]]; then
   callApi "/api/servers/spring-boot/mvc/web/tomcat"
   callApi "/api/servers/spring-boot/mvc/springdoc/init"
   callApi "/api/servers/spring-boot/databases/mysql"
-  callApi "/api/servers/spring-boot/databases/migration/liquibase"
+  callApi "/api/servers/spring-boot/databases/migration/flyway/init"
   callApi "/api/servers/spring-boot/cache/ehcache/xml-configuration"
-
+0
 elif [[ $filename == 'undertow-simplecache' ]]; then
   callApi "/api/projects/init"
   callApi "/api/build-tools/maven"

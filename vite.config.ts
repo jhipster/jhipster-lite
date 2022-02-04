4473d40aad90372850192c51ec47f72796f745f4
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-02-04 11:14:34 +0100

    Add Vite with Vue3

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: '../../../target/classes/static',
+  },
+  root: 'src/main/webapp',
+});

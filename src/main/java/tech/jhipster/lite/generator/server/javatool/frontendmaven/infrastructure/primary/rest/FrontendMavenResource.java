53154c2f063cf64a0f6e98017acd30dc75b5f31a
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-02-07 18:15:41 +0100

    Frontend Maven Plugin: add feature

---
+++ src/main/java/tech/jhipster/lite/generator/server/javatool/frontendmaven/infrastructure/primary/rest/FrontendMavenResource.java
@@ -0,0 +1,34 @@
+package tech.jhipster.lite.generator.server.javatool.frontendmaven.infrastructure.primary.rest;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.jhipster.lite.generator.project.domain.Project;
+import tech.jhipster.lite.generator.project.infrastructure.primary.dto.ProjectDTO;
+import tech.jhipster.lite.generator.server.javatool.frontendmaven.application.FrontendMavenApplicationService;
+import tech.jhipster.lite.technical.infrastructure.primary.annotation.GeneratorStep;
+
+@RestController
+@RequestMapping("/api/frontend-maven-plugin")
+@Tag(name = "Frontend Maven Plugin")
+class FrontendMavenResource {
+
+  private final FrontendMavenApplicationService frontendMavenApplicationService;
+
+  public FrontendMavenResource(FrontendMavenApplicationService frontendMavenApplicationService) {
+    this.frontendMavenApplicationService = frontendMavenApplicationService;
+  }
+
+  @Operation(summary = "Init", description = "Add Frontend Maven Plugin")
+  @ApiResponse(responseCode = "500", description = "An error occurred while adding Frontend Maven Plugin")
+  @PostMapping
+  @GeneratorStep(id = "frontend-maven-plugin")
+  public void addFrontendMavenPlugin(@RequestBody ProjectDTO projectDTO) {
+    Project project = ProjectDTO.toProject(projectDTO);
+    frontendMavenApplicationService.addFrontendMavenPlugin(project);
+  }
+}
---
+++ src/test/java/tech/jhipster/lite/generator/server/javatool/frontendmaven/infrastructure/primary/rest/FrontendMavenResourceIT.java
@@ -0,0 +1,54 @@
+package tech.jhipster.lite.generator.server.javatool.frontendmaven.infrastructure.primary.rest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static tech.jhipster.lite.TestUtils.tmpProjectWithPomXml;
+import static tech.jhipster.lite.common.domain.FileUtils.tmpDirForTest;
+import static tech.jhipster.lite.generator.server.javatool.frontendmaven.application.FrontendMavenAssert.*;
+
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import tech.jhipster.lite.IntegrationTest;
+import tech.jhipster.lite.TestUtils;
+import tech.jhipster.lite.generator.buildtool.maven.application.MavenApplicationService;
+import tech.jhipster.lite.generator.init.application.InitApplicationService;
+import tech.jhipster.lite.generator.project.domain.Project;
+import tech.jhipster.lite.generator.project.infrastructure.primary.dto.ProjectDTO;
+import tech.jhipster.lite.generator.server.javatool.frontendmaven.application.FrontendMavenAssert;
+
+@IntegrationTest
+@AutoConfigureMockMvc
+class FrontendMavenResourceIT {
+
+  @Autowired
+  InitApplicationService initApplicationService;
+
+  @Autowired
+  MavenApplicationService mavenApplicationService;
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  void shouldAddFrontendMavenPlugin() throws Exception {
+    ProjectDTO projectDTO = TestUtils.readFileToObject("json/chips.json", ProjectDTO.class).folder(tmpDirForTest());
+    Project project = ProjectDTO.toProject(projectDTO);
+    initApplicationService.init(project);
+    mavenApplicationService.init(project);
+
+    mockMvc
+      .perform(
+        post("/api/frontend-maven-plugin").contentType(MediaType.APPLICATION_JSON).content(TestUtils.convertObjectToJsonBytes(projectDTO))
+      )
+      .andExpect(status().isOk());
+
+    assertChecksumMavenPlugin(project);
+    assertMavenAntrunPlugin(project);
+    assertFrontendMavenPlugin(project);
+  }
+}

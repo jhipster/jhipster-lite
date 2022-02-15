f7e3fd71029312e67fe52d0893ad00a3f0263de0
Arnaud Freismuth <arnaudfreismuth@gmail.com>
2022-02-15 10:39:33 +0100

    Add axios http in infrastructure layer

---
+++ src/main/webapp/app/http/AxiosHttp.ts
@@ -0,0 +1,21 @@
+import { AxiosInstance, AxiosResponse } from 'axios';
+
+export class AxiosHttp {
+  constructor(private axiosInstance: AxiosInstance) {}
+
+  async get<Result>(uri: string): Promise<AxiosResponse<Result>> {
+    return this.axiosInstance.get<Result>(uri);
+  }
+
+  async put<Result, Payload = never>(uri: string, data?: Payload): Promise<AxiosResponse<Result>> {
+    return this.axiosInstance.put<Result>(uri, data);
+  }
+
+  async post<Result, Payload = never>(uri: string, data?: Payload): Promise<AxiosResponse<Result>> {
+    return this.axiosInstance.post<Result>(uri, data);
+  }
+
+  async delete<Result>(uri: string): Promise<AxiosResponse<Result>> {
+    return this.axiosInstance.delete<Result>(uri);
+  }
+}

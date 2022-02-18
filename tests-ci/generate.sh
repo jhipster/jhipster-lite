1707b98f7fd191935d5b9b776ded9fefd860e462
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-02-18 20:03:21 +0100

    CI: update with vueapp

--- tests-ci/generate.sh
+++ tests-ci/generate.sh
@@ -50,6 +50,16 @@ if [[ $filename == 'full-default' ]]; then
 
   callApi "/api/frontend-maven-plugin"
   callApi "/api/vite/vue"
+
+elif [[ $filename == 'vueapp' ]]; then
+  callApi "/api/projects/init"
+  callApi "/api/build-tools/maven"
+  callApi "/api/servers/java/base"
+  callApi "/api/servers/java/jacoco-minimum-coverage"
+  callApi "/api/servers/spring-boot"
+  callApi "/api/servers/spring-boot/mvc/web/tomcat"
+
+  callApi "/api/frontend-maven-plugin"
   callApi "/api/vite/vue/styled"
 
 elif [[ $filename == 'tomcat-mysql-ehcachexml' ]]; then

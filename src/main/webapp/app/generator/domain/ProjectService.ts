b8ffa66fadccbe4cd2c649b444260af173993f54
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-03-05 18:34:03 +0100

    Front: add addMaven to secondary

--- src/main/webapp/app/generator/domain/ProjectService.ts
+++ src/main/webapp/app/generator/domain/ProjectService.ts
@@ -2,4 +2,6 @@ import { Project } from '@/generator/domain/Project';
 
 export interface ProjectService {
   init(project: Project): Promise<void>;
+
+  addMaven(project: Project): Promise<void>;
 }
--- src/test/javascript/spec/generator/secondary/ProjectRepository.spec.ts
+++ src/test/javascript/spec/generator/secondary/ProjectRepository.spec.ts
@@ -15,4 +15,17 @@ describe('ProjectsRepository', () => {
     expect(uri).toBe('api/projects/init');
     expect(payload).toEqual<Project>(project);
   });
+
+  it('Should add Maven', () => {
+    const axiosHttpStub = stubAxiosHttp();
+    axiosHttpStub.post.resolves();
+    const projectRepository = new ProjectRepository(axiosHttpStub);
+    const project: Project = { folder: 'folder/path' };
+
+    projectRepository.addMaven(project);
+
+    const [uri, payload] = axiosHttpStub.post.getCall(0).args;
+    expect(uri).toBe('api/build-tools/maven');
+    expect(payload).toEqual<Project>(project);
+  });
 });

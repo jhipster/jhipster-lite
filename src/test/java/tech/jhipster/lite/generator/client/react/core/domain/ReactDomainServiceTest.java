809a14b0e1f98f3359e13166c557b3c03bdec21e
Quentin Monmert <quentin.monmert@gmail.com>
2022-03-06 21:21:14 +0100

    [React] Remove wildcard imports

--- src/test/java/tech/jhipster/lite/generator/client/react/core/domain/ReactDomainServiceTest.java
+++ src/test/java/tech/jhipster/lite/generator/client/react/core/domain/ReactDomainServiceTest.java
@@ -3,7 +3,9 @@ package tech.jhipster.lite.generator.client.react.core.domain;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static tech.jhipster.lite.TestUtils.tmpProject;
 
 import java.util.Optional;

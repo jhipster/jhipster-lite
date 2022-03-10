44b4d2063b799acb1665184b8d33df6b2249e4a7
seraphinandrieux <sandrieux@ippon.fr>
2022-03-10 10:24:22 +0100

    Create History API

---
+++ src/main/java/tech/jhipster/lite/generator/history/infrastructure/primary/HistoryResource.java
@@ -0,0 +1,36 @@
+package tech.jhipster.lite.generator.history.infrastructure.primary;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tech.jhipster.lite.generator.history.application.GeneratorHistoryApplicationService;
+import tech.jhipster.lite.generator.history.infrastructure.primary.dto.HistoryDTO;
+import tech.jhipster.lite.generator.project.domain.Project;
+
+@RestController
+@RequestMapping("/api/projects")
+@Tag(name = "History")
+class HistoryResource {
+
+  private final GeneratorHistoryApplicationService generatorHistoryApplicationService;
+
+  public HistoryResource(GeneratorHistoryApplicationService generatorHistoryApplicationService) {
+    this.generatorHistoryApplicationService = generatorHistoryApplicationService;
+  }
+
+  @Operation(summary = "Get project history")
+  @ApiResponse(responseCode = "500", description = "An error occurred while getting history project")
+  @GetMapping("/history")
+  public ResponseEntity<HistoryDTO> get(
+    @Parameter(description = "Project path to get history") @RequestParam(value = "folder") String folder
+  ) {
+    Project project = Project.builder().folder(folder).build();
+    return ResponseEntity.ok(HistoryDTO.from(generatorHistoryApplicationService.getValues(project)));
+  }
+}
---
+++ src/test/java/tech/jhipster/lite/generator/history/application/GeneratorHistoryApplicationServiceTest.java
@@ -0,0 +1,43 @@
+package tech.jhipster.lite.generator.history.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static tech.jhipster.lite.TestUtils.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tech.jhipster.lite.UnitTest;
+import tech.jhipster.lite.error.domain.MissingMandatoryValueException;
+import tech.jhipster.lite.generator.history.domain.GeneratorHistoryData;
+import tech.jhipster.lite.generator.history.domain.GeneratorHistoryRepository;
+import tech.jhipster.lite.generator.project.domain.Project;
+
+@UnitTest
+@ExtendWith(MockitoExtension.class)
+class GeneratorHistoryApplicationServiceTest {
+
+  @InjectMocks
+  GeneratorHistoryApplicationService generatorHistoryApplicationService;
+
+  @Mock
+  GeneratorHistoryRepository generatorHistoryRepository;
+
+  @Test
+  void shouldNotGetHistoryWithoutProject() {
+    assertThatThrownBy(() -> generatorHistoryApplicationService.getValues(null))
+      .isExactlyInstanceOf(MissingMandatoryValueException.class)
+      .hasMessageContaining("project");
+  }
+
+  @Test
+  void shouldGetHistory() {
+    final Project project = tmpProject();
+    final GeneratorHistoryData historyData = new GeneratorHistoryData();
+    when(generatorHistoryRepository.getHistoryData(project)).thenReturn(historyData);
+
+    assertThat(generatorHistoryApplicationService.getValues(project)).isEqualTo(historyData.getValues());
+  }
+}

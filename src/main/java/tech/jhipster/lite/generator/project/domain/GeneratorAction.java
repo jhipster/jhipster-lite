5533912f87d3a2c07f77ebb65b21e5d34b2d41d2
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-04-02 18:00:46 +0200

    Add constants: GeneratorAction

---
+++ src/main/java/tech/jhipster/lite/generator/project/domain/GeneratorAction.java
@@ -0,0 +1,10 @@
+package tech.jhipster.lite.generator.project.domain;
+
+public class GeneratorAction {
+
+  private GeneratorAction() {}
+
+  // init
+  public static final String INIT = "init";
+  public static final String DOWNLOAD = "download";
+}

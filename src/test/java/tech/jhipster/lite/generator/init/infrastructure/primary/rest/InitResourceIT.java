18a758c3cddbd705ddda82ff502d50faee72273e
Quentin Monmert <quentin.monmert@gmail.com>
2022-04-10 20:31:45 +0200

    Remove unused import

--- src/test/java/tech/jhipster/lite/generator/init/infrastructure/primary/rest/InitResourceIT.java
+++ src/test/java/tech/jhipster/lite/generator/init/infrastructure/primary/rest/InitResourceIT.java
@@ -14,13 +14,11 @@ import static tech.jhipster.lite.generator.project.domain.Constants.PACKAGE_JSON
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import tech.jhipster.lite.IntegrationTest;
 import tech.jhipster.lite.common.domain.FileUtils;
 import tech.jhipster.lite.generator.init.application.InitApplicationService;
-import tech.jhipster.lite.generator.packagemanager.npm.domain.NpmRepository;
 import tech.jhipster.lite.generator.project.domain.Project;
 import tech.jhipster.lite.generator.project.infrastructure.primary.dto.ProjectDTO;
 

b81d780d72952a5ec4bf16ce2de59e7a0b313156
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-04-17 17:32:27 +0200

    Front: upgrade footer

--- src/main/webapp/app/springboot/primary/Generator.vue
+++ src/main/webapp/app/springboot/primary/Generator.vue
@@ -646,32 +646,11 @@
   </main>
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div class="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+      <a href="https://www.jhipster.tech/" target="_blank" rel="noopener" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
         <img src="../../../content/JHipster-Lite-neon-blue_40x.png" alt="" />
       </a>
       <span class="text-muted">© 2022 JHipster Lite</span>
     </div>
-
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li class="ms-3">
-        <a class="text-muted" href="#"
-          ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
-            <path
-              d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"
-            /></svg
-        ></a>
-      </li>
-      <li class="ms-3">
-        <a class="text-muted" href="#"
-          ><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg
-        ></a>
-      </li>
-      <li class="ms-3">
-        <a class="text-muted" href="#"
-          ><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg
-        ></a>
-      </li>
-    </ul>
   </footer>
 </template>
 

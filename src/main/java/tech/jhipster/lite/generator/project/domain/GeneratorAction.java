af50f3187cb381db54a9c35e6eddbe5b3dfd07e1
François Delbrayelle <fdelbrayelle@gmail.com>
2022-04-30 23:18:46 +0200

    Update GeneratorAction.java

--- src/main/java/tech/jhipster/lite/generator/project/domain/GeneratorAction.java
+++ src/main/java/tech/jhipster/lite/generator/project/domain/GeneratorAction.java
@@ -113,7 +113,7 @@ public class GeneratorAction {
   public static final String REACT_CYPRESS = "react-cypress";
 
   public static final String SPRINGBOOT_KAFKA = "springboot-kafka";
-  public static final String SPRINGBOOT_KAFKA_DUMMY_PRODUCER_CONSUMER = "springboot-kafka-dummy-producer";
+  public static final String SPRINGBOOT_KAFKA_DUMMY_PRODUCER_CONSUMER = "springboot-kafka-dummy-producer-consumer";
   public static final String SPRINGBOOT_KAFKA_AKHQ = "springboot-kafka-akhq";
 
   public static final String VUE = "vue";

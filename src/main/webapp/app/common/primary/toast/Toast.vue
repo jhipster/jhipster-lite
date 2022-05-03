29a61bb2f0697d5610af57ac5fe10d207c663204
Swaraj Pal <swarajpalsaaj@yahoo.co.in>
2022-05-03 08:37:35 +0530

    Fix Sonar bug in Toast
    Fix #1397

--- src/main/webapp/app/common/primary/toast/Toast.vue
+++ src/main/webapp/app/common/primary/toast/Toast.vue
@@ -9,7 +9,7 @@
   >
     <div class="d-flex">
       <div class="toast-body">
-        <b>{{ message }}</b>
+        <strong>{{ message }}</strong>
       </div>
       <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>

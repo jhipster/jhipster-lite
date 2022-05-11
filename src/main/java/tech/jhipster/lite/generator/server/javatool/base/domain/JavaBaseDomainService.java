0a541706f4faed17143d3cb1c16329eb12593fd7
Colin DAMON <cdamon@ippon.fr>
2022-05-11 18:15:56 +0200

    Simple collections utility
    The main goal here is to init a Collection manipulation utility class
    for the project. It's always usefull to have this kind of null safe
    utility

--- src/main/java/tech/jhipster/lite/generator/server/javatool/base/domain/JavaBaseDomainService.java
+++ src/main/java/tech/jhipster/lite/generator/server/javatool/base/domain/JavaBaseDomainService.java
@@ -5,6 +5,7 @@ import static tech.jhipster.lite.generator.project.domain.Constants.MAIN_JAVA;
 import static tech.jhipster.lite.generator.project.domain.Constants.TEST_JAVA;
 import static tech.jhipster.lite.generator.project.domain.DefaultConfig.PACKAGE_NAME;
 
+import tech.jhipster.lite.common.domain.WordUtils;
 import tech.jhipster.lite.generator.project.domain.DefaultConfig;
 import tech.jhipster.lite.generator.project.domain.Project;
 import tech.jhipster.lite.generator.project.domain.ProjectRepository;
@@ -13,6 +14,7 @@ public class JavaBaseDomainService implements JavaBaseService {
 
   public static final String SOURCE = "server/javatool/base";
   public static final String ERROR_DOMAIN_PATH = "error/domain";
+  private static final String COMMON_DOMAIN_PATH = "common/domain";
 
   private final ProjectRepository projectRepository;
 
@@ -25,6 +27,12 @@ public class JavaBaseDomainService implements JavaBaseService {
     project.addDefaultConfig(PACKAGE_NAME);
     String packageNamePath = project.getPackageNamePath().orElse(DefaultConfig.PACKAGE_PATH);
 
+    addErrors(project, packageNamePath);
+    addCollections(project, packageNamePath);
+    addAnnotations(project, packageNamePath);
+  }
+
+  private void addErrors(Project project, String packageNamePath) {
     JavaBase
       .errorDomainFiles()
       .forEach(file -> projectRepository.template(project, SOURCE, file, getPath(MAIN_JAVA, packageNamePath, ERROR_DOMAIN_PATH)));
@@ -32,7 +40,29 @@ public class JavaBaseDomainService implements JavaBaseService {
     JavaBase
       .errorDomainTestFiles()
       .forEach(file -> projectRepository.template(project, SOURCE, file, getPath(TEST_JAVA, packageNamePath, ERROR_DOMAIN_PATH)));
+  }
 
+  private void addCollections(Project project, String packageNamePath) {
+    String className = WordUtils.upperFirst(project.getBaseName().orElse("jhipster"));
+    project.addConfig("collectionClass", className);
+
+    projectRepository.template(
+      project,
+      SOURCE,
+      "ProjectCollections.java",
+      getPath(MAIN_JAVA, packageNamePath, COMMON_DOMAIN_PATH),
+      className + "Collections.java"
+    );
+    projectRepository.template(
+      project,
+      SOURCE,
+      "ProjectCollectionsTest.java",
+      getPath(TEST_JAVA, packageNamePath, COMMON_DOMAIN_PATH),
+      className + "CollectionsTest.java"
+    );
+  }
+
+  private void addAnnotations(Project project, String packageNamePath) {
     JavaBase.annotationsFiles().forEach(file -> projectRepository.template(project, SOURCE, file, getPath(TEST_JAVA, packageNamePath)));
   }
 }

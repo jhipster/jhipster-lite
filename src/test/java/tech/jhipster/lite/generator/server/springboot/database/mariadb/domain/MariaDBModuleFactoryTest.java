b2d8e7be5b2f2d9812e1cf5db3f9866f9b59f8c4
Daniel Franco <dandrfranco@gmail.com>
2022-06-19 20:05:25 +0100

    Validate sql common properties

--- src/test/java/tech/jhipster/lite/generator/server/springboot/database/mariadb/domain/MariaDBModuleFactoryTest.java
+++ src/test/java/tech/jhipster/lite/generator/server/springboot/database/mariadb/domain/MariaDBModuleFactoryTest.java
@@ -1,6 +1,8 @@
 package tech.jhipster.lite.generator.server.springboot.database.mariadb.domain;
 
 import static org.mockito.Mockito.when;
+import static tech.jhipster.lite.generator.module.domain.JHipsterModule.propertyKey;
+import static tech.jhipster.lite.generator.module.domain.JHipsterModule.propertyValue;
 import static tech.jhipster.lite.generator.module.infrastructure.secondary.JHipsterModulesAssertions.assertThatModuleOnProjectWithDefaultPom;
 
 import org.junit.jupiter.api.Test;
@@ -53,11 +55,37 @@ class MariaDBModuleFactoryTest {
       .containing("<artifactId>HikariCP</artifactId>")
       .containing("<groupId>org.hibernate</groupId>")
       .containing("<artifactId>hibernate-core</artifactId>")
+      .containing("<groupId>org.testcontainers</groupId>")
+      .containing("<artifactId>mariadb</artifactId>")
       .and()
       .createFile("src/main/resources/config/application.properties")
       .containing("spring.datasource.url=jdbc:mariadb://localhost:3306/myapp")
       .containing("spring.datasource.username=root")
       .containing("spring.datasource.password=")
-      .containing("spring.datasource.driver-class-name=org.mariadb.jdbc.Driver");
+      .containing("spring.datasource.driver-class-name=org.mariadb.jdbc.Driver")
+      .containing("spring.datasource.type=com.zaxxer.hikari.HikariDataSource")
+      .containing("spring.datasource.hikari.poolName=Hikari")
+      .containing("spring.datasource.hikari.auto-commit=false")
+      .containing("spring.data.jpa.repositories.bootstrap-mode=deferred")
+      .containing("spring.jpa.hibernate.ddl-auto=none")
+      .containing("spring.jpa.hibernate.naming.implicit-strategy=org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy")
+      .containing("spring.jpa.hibernate.naming.physical-strategy=org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy")
+      .containing("spring.jpa.open-in-view=false")
+      .containing("spring.jpa.properties.hibernate.connection.provider_disables_autocommit=true")
+      .containing("spring.jpa.properties.hibernate.generate_statistics=false")
+      .containing("spring.jpa.properties.hibernate.id.new_generator_mappings=true")
+      .containing("spring.jpa.properties.hibernate.jdbc.batch_size=25")
+      .containing("spring.jpa.properties.hibernate.jdbc.time_zone=UTC")
+      .containing("spring.jpa.properties.hibernate.order_inserts=true")
+      .containing("spring.jpa.properties.hibernate.order_updates=true")
+      .containing("spring.jpa.properties.hibernate.query.fail_on_pagination_over_collection_fetch=true")
+      .containing("spring.jpa.properties.hibernate.query.in_clause_parameter_padding=true")
+      .and()
+      .createFile("src/test/resources/config/application.properties")
+      .containing("spring.datasource.url=jdbc:tc:mariadb")
+      .containing("spring.datasource.username=myapp")
+      .containing("spring.datasource.password=")
+      .containing("spring.datasource.driver-class-name=org.testcontainers.jdbc.ContainerDatabaseDriver")
+      .containing("spring.datasource.hikari.maximum-pool-size=2");
   }
 }

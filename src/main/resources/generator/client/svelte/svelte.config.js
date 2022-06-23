1360c3fe9326fa491898683887686e0083e53fb5
Florian Carre <fcarre@ippon.fr>
2022-06-23 22:24:24 +0200

    fix: generate svelte && cypress project crash

--- src/main/resources/generator/client/svelte/svelte.config.js
+++ src/main/resources/generator/client/svelte/svelte.config.js
@@ -2,10 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import path from 'path';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: preprocess(),
 
   kit: {

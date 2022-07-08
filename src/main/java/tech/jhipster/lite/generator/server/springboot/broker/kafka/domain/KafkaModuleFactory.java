62e34fc82f34713d1bdd026ef21a99237690bd8d
François Delbrayelle <fdelbrayelle@gmail.com>
2022-07-08 11:23:14 +0200

    feat(kafka): modules - add properties

--- src/main/java/tech/jhipster/lite/generator/server/springboot/broker/kafka/domain/KafkaModuleFactory.java
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/broker/kafka/domain/KafkaModuleFactory.java
@@ -1,11 +1,11 @@
 package tech.jhipster.lite.generator.server.springboot.broker.kafka.domain;
 
+import static tech.jhipster.lite.module.domain.JHipsterModule.*;
+
 import tech.jhipster.lite.docker.domain.DockerImages;
 import tech.jhipster.lite.module.domain.JHipsterModule;
 import tech.jhipster.lite.module.domain.JHipsterSource;
 import tech.jhipster.lite.module.domain.properties.JHipsterModuleProperties;
-
-import static tech.jhipster.lite.module.domain.JHipsterModule.*;
 
 public class KafkaModuleFactory {
 
@@ -29,6 +29,16 @@ public class KafkaModuleFactory {
         .and()
       .files()
         .add(SOURCE.template("kafka.yml"), toSrcMainDocker().append("kafka.yml"))
+        .and()
+      .springMainProperties()
+        .set(propertyKey("kafka.bootstrap-servers"), propertyValue("localhost:9092"))
+        .set(propertyKey("kafka.consumer.key.deserializer"), propertyValue("org.apache.kafka.common.serialization.StringDeserializer"))
+        .set(propertyKey("kafka.consumer.value.deserializer"), propertyValue("org.apache.kafka.common.serialization.StringDeserializer"))
+        .set(propertyKey("kafka.consumer.group.id"), propertyValue("myapp"))
+        .set(propertyKey("kafka.consumer.auto.offset.reset"), propertyValue("earliest"))
+        .set(propertyKey("kafka.producer.key.serializer"), propertyValue("org.apache.kafka.common.serialization.StringSerializer"))
+        .set(propertyKey("kafka.producer.value.serializer"), propertyValue("org.apache.kafka.common.serialization.StringSerializer"))
+        .set(propertyKey("kafka.polling.timeout"), propertyValue("10000"))
         .and();
     //@formatter:on
 
--- src/test/java/tech/jhipster/lite/generator/server/springboot/broker/kafka/domain/KafkaModuleFactoryTest.java
+++ src/test/java/tech/jhipster/lite/generator/server/springboot/broker/kafka/domain/KafkaModuleFactoryTest.java
@@ -2,6 +2,8 @@ package tech.jhipster.lite.generator.server.springboot.broker.kafka.domain;
 
 import static org.mockito.Mockito.when;
 import static tech.jhipster.lite.TestFileUtils.tmpDirForTest;
+import static tech.jhipster.lite.generator.project.domain.Constants.MAIN_DOCKER;
+import static tech.jhipster.lite.generator.project.domain.Constants.MAIN_RESOURCES;
 import static tech.jhipster.lite.module.infrastructure.secondary.JHipsterModulesAssertions.assertThatModuleWithFiles;
 import static tech.jhipster.lite.module.infrastructure.secondary.JHipsterModulesAssertions.pomFile;
 
@@ -32,7 +34,11 @@ class KafkaModuleFactoryTest {
     when(dockerImages.get("confluentinc/cp-zookeeper")).thenReturn(new DockerImage("confluentinc/cp-zookeeper", "1.0.0"));
     when(dockerImages.get("confluentinc/cp-kafka")).thenReturn(new DockerImage("confluentinc/cp-kafka", "1.0.0"));
 
-    JHipsterModuleProperties properties = JHipsterModulesFixture.propertiesBuilder(tmpDirForTest()).build();
+    JHipsterModuleProperties properties = JHipsterModulesFixture
+      .propertiesBuilder(tmpDirForTest())
+      .basePackage("com.jhipster.test")
+      .projectBaseName("myapp")
+      .build();
 
     JHipsterModule module = factory.buildModule(properties);
 
@@ -40,13 +46,24 @@ class KafkaModuleFactoryTest {
       .createFile("pom.xml")
       .containing(
         """
-              <dependency>
-                <groupId>org.apache.kafka</groupId>
-                <artifactId>kafka-clients</artifactId>
-              </dependency>
-          """
+                <dependency>
+                  <groupId>org.apache.kafka</groupId>
+                  <artifactId>kafka-clients</artifactId>
+                </dependency>
+            """
       )
       .and()
-      .createFile("src/main/docker/kafka.yml");
+      .createFile(MAIN_DOCKER + "/kafka.yml")
+      .and()
+      .createFile(MAIN_RESOURCES + "/config/application.properties")
+      .containing("kafka.bootstrap-servers=localhost:9092")
+      .containing("kafka.consumer.key.deserializer=org.apache.kafka.common.serialization.StringDeserializer")
+      .containing("kafka.consumer.value.deserializer=org.apache.kafka.common.serialization.StringDeserializer")
+      .containing("kafka.consumer.group.id=myapp")
+      .containing("kafka.consumer.auto.offset.reset=earliest")
+      .containing("kafka.producer.key.serializer=org.apache.kafka.common.serialization.StringSerializer")
+      .containing("kafka.producer.value.serializer=org.apache.kafka.common.serialization.StringSerializer")
+      .containing("kafka.polling.timeout=10000")
+      .and();
   }
 }

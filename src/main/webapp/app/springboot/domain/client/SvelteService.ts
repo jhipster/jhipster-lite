7bf7f4b536d1bdf2357a64d930f3b1d279bf7ddb
Colin DAMON <cdamon@ippon.fr>
2022-07-16 15:35:01 +0200

    Remove svelte style checkbox

--- src/main/webapp/app/springboot/domain/client/SvelteService.ts
+++ src/main/webapp/app/springboot/domain/client/SvelteService.ts
@@ -1,6 +1,5 @@
 import { Project } from '@/springboot/domain/Project';
 
 export interface SvelteService {
-  add(project: Project): Promise<void>;
   addWithStyle(project: Project): Promise<void>;
 }
--- src/main/webapp/app/springboot/primary/generator/svelte-generator/SvelteGenerator.vue
+++ src/main/webapp/app/springboot/primary/generator/svelte-generator/SvelteGenerator.vue
@@ -1,20 +1,5 @@
 <template>
   <div id="v-pills-svelte" class="tab-pane fade" role="tabpanel" aria-labelledby="v-pills-svelte-tab">
-    <div class="list-group--inline py-2">
-      <label for="svelte-with-style" class="list-group-item gap-3">
-        <input
-          id="svelte-with-style"
-          v-model="isSvelteWithStyle"
-          type="checkbox"
-          name="svelte-with-style"
-          class="form-check-input flex-shrink-0"
-          value=""
-        />
-        <span class="form-checked-content">
-          <strong>Add Svelte style</strong>
-        </span>
-      </label>
-    </div>
     <DefaultButtonVue
       id="svelte"
       :label="'Generate Svelte'"

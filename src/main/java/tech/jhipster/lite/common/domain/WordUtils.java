b576e84b9cf7a11d391221656702c9fc90785732
Colin DAMON <cdamon@ippon.fr>
2022-07-25 08:13:17 +0200

    Remove unused from WordUtils

--- src/main/java/tech/jhipster/lite/common/domain/WordUtils.java
+++ src/main/java/tech/jhipster/lite/common/domain/WordUtils.java
@@ -1,6 +1,5 @@
 package tech.jhipster.lite.common.domain;
 
-import com.google.common.base.CaseFormat;
 import tech.jhipster.lite.error.domain.Assert;
 
 public class WordUtils {
@@ -18,24 +17,7 @@ public class WordUtils {
   public static final String O_BRACKET = "[";
   public static final String C_BRACKET = "]";
 
-  private static final String VALUE_FIELD = "value";
-
   private WordUtils() {}
-
-  public static String kebabCase(String value) {
-    Assert.notBlank(VALUE_FIELD, value);
-
-    return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_HYPHEN, value);
-  }
-
-  public static String upperFirst(String value) {
-    Assert.notBlank(VALUE_FIELD, value);
-
-    StringBuilder result = new StringBuilder();
-    result.append(value.substring(0, 1).toUpperCase());
-    result.append(value.substring(1));
-    return result.toString();
-  }
 
   public static String indent(int times) {
     return indent(times, DEFAULT_INDENTATION);

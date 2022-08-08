a73b2b4dd2e7c66cbe0f0e3763d7cdf83d00f8a6
Colin DAMON <cdamon@ippon.fr>
2022-08-08 13:35:09 +0200

    Remove unused default button

--- src/main/webapp/app/common/primary/default-button/DefaultButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <button class="btn" :class="outlined ? 'btn-outline-primary' : 'btn-primary'" :disabled="disabled">
-    <IconVue v-if="icon" class="me-1" :name="icon" :aria-hidden="true" />
-    {{ label }}
-  </button>
-</template>
-
-<script lang="ts" src="./DefaultButton.component.ts"></script>

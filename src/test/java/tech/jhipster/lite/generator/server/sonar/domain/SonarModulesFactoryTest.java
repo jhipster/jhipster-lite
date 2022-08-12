4e07bf0378fe50ea134bbb0157b4c28bb5b6226a
Colin DAMON <cdamon@ippon.fr>
2022-08-12 09:50:12 +0200

    Add sonar startup commands

--- src/test/java/tech/jhipster/lite/generator/server/sonar/domain/SonarModulesFactoryTest.java
+++ src/test/java/tech/jhipster/lite/generator/server/sonar/domain/SonarModulesFactoryTest.java
@@ -74,7 +74,7 @@ class SonarModulesFactoryTest {
   }
 
   private ModuleAsserter assertCommonModule(JHipsterModule module) {
-    return assertThatModuleWithFiles(module, pomFile())
+    return assertThatModuleWithFiles(module, pomFile(), readmeFile())
       .createFile("pom.xml")
       .containing(
         """
@@ -110,6 +110,13 @@ class SonarModulesFactoryTest {
       .and()
       .createFile("src/main/docker/sonar.yml")
       .containing("sonarqube:1.1.1")
+      .and()
+      .createFile("documentation/sonar.md")
+      .containing("docker-compose -f src/main/docker/sonar.yml up -d")
+      .and()
+      .createFile("README.md")
+      .containing("docker-compose -f src/main/docker/sonar.yml up -d")
+      .containing("./mvnw clean verify sonar:sonar")
       .and();
   }
 }

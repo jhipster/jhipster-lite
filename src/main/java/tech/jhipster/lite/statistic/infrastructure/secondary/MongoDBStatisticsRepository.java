ac91b3d58180eb942c206771086f5dfada60b1c2
Colin DAMON <damon.colin@gmail.com>
2022-08-24 09:56:31 +0200

    Lower mongo instance memory to enable tests

--- src/main/java/tech/jhipster/lite/statistic/infrastructure/secondary/MongoDBStatisticsRepository.java
+++ src/main/java/tech/jhipster/lite/statistic/infrastructure/secondary/MongoDBStatisticsRepository.java
@@ -1,7 +1,6 @@
 package tech.jhipster.lite.statistic.infrastructure.secondary;
 
 import org.springframework.stereotype.Repository;
-import tech.jhipster.lite.common.domain.Generated;
 import tech.jhipster.lite.error.domain.Assert;
 import tech.jhipster.lite.statistic.domain.AppliedModule;
 import tech.jhipster.lite.statistic.domain.Statistics;
@@ -9,7 +8,6 @@ import tech.jhipster.lite.statistic.domain.StatisticsRepository;
 
 @WithMongo
 @Repository
-@Generated(reason = "Starting a mongo takes a lot of time so tests for this are basically disabled")
 class MongoDBStatisticsRepository implements StatisticsRepository {
 
   private final SpringDataMongoDbStatisticsRepository statistics;
--- src/main/java/tech/jhipster/lite/statistic/infrastructure/secondary/MongockConfiguration.java
+++ src/main/java/tech/jhipster/lite/statistic/infrastructure/secondary/MongockConfiguration.java
@@ -3,11 +3,9 @@ package tech.jhipster.lite.statistic.infrastructure.secondary;
 import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Configuration;
-import tech.jhipster.lite.common.domain.Generated;
 
 @WithMongo
 @Configuration
 @EnableMongock
 @AutoConfigureAfter(MongoDBConfiguration.class)
-@Generated(reason = "Not testing it since starting a mongo cost a lot of time")
 class MongockConfiguration {}

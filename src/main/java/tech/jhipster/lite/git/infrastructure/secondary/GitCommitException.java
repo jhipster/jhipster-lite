4edc95f539f94ea76ff1a541048d28e16b21da33
Colin DAMON <damon.colin@gmail.com>
2022-08-28 11:52:46 +0200

    Remove unused constructors

--- src/main/java/tech/jhipster/lite/git/infrastructure/secondary/GitCommitException.java
+++ src/main/java/tech/jhipster/lite/git/infrastructure/secondary/GitCommitException.java
@@ -1,14 +1,8 @@
 package tech.jhipster.lite.git.infrastructure.secondary;
 
-import tech.jhipster.lite.common.domain.Generated;
 import tech.jhipster.lite.error.domain.GeneratorException;
 
-@Generated
 class GitCommitException extends GeneratorException {
-
-  public GitCommitException(String message) {
-    super(message);
-  }
 
   public GitCommitException(String message, Throwable cause) {
     super(message, cause);
--- src/main/java/tech/jhipster/lite/git/infrastructure/secondary/GitInitException.java
+++ src/main/java/tech/jhipster/lite/git/infrastructure/secondary/GitInitException.java
@@ -1,14 +1,8 @@
 package tech.jhipster.lite.git.infrastructure.secondary;
 
-import tech.jhipster.lite.common.domain.Generated;
 import tech.jhipster.lite.error.domain.GeneratorException;
 
-@Generated
 class GitInitException extends GeneratorException {
-
-  public GitInitException(String message) {
-    super(message);
-  }
 
   public GitInitException(String message, Throwable cause) {
     super(message, cause);

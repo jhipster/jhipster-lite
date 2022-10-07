9f890ed3b2fe1ee2866376de351e305cdce0f86f
Quentin Monmert <quentin.monmert@gmail.com>
2022-10-07 16:58:01 +0200

    React: Move to vitest (fix)

--- src/main/resources/generator/client/react/vitest.config.ts
+++ src/main/resources/generator/client/react/vitest.config.ts
@@ -10,8 +10,10 @@ export default defineConfig({
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src/main/webapp/app') }],
   },
   test: {
-    reporters: 'vitest-sonar-reporter',
-    outputFile: 'target/test-results/jest/TESTS-results-sonar.xml',
+    reporters: ['json', 'verbose', 'vitest-sonar-reporter'],
+    outputFile: {
+      'vitest-sonar-reporter': 'target/test-results/jest/TESTS-results-sonar.xml',
+    },
     globals: true,
     logHeapUsage: true,
     minThreads: 1,
@@ -21,16 +23,16 @@ export default defineConfig({
     include: ['src/test/javascript/spec/**/*.(spec|test).(ts|tsx)'],
     exclude: ['node_modules', 'src/test/javascript/integration/**/*.spec.ts'],
     coverage: {
-        clean: true,
-        provider: 'istanbul',
-        reportsDirectory: 'target/test-results/',
-        reporter: ['html', 'json-summary', 'text', 'text-summary', 'lcov', 'clover'],
-        watermarks: {
-          statements: [100, 100],
-          branches: [100, 100],
-          functions: [100, 100],
-          lines: [100, 100],
-        },
+      clean: true,
+      provider: 'istanbul',
+      reportsDirectory: 'target/test-results/',
+      reporter: ['html', 'json-summary', 'text', 'text-summary', 'lcov', 'clover'],
+      watermarks: {
+        statements: [100, 100],
+        branches: [100, 100],
+        functions: [100, 100],
+        lines: [100, 100],
       },
+    },
   },
 });

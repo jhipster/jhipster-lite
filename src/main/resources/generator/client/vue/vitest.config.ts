16b7f4dfe424d46cc2e5f81bc89226e46bef7b37
Quentin Monmert <quentin.monmert@gmail.com>
2022-10-08 14:37:06 +0200

    Vue: move to Vitest

---
+++ src/main/resources/generator/client/vue/vitest.config.ts
@@ -0,0 +1,38 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src/main/webapp/app') }],
+  },
+  test: {
+    reporters: ['json', 'verbose', 'vitest-sonar-reporter'],
+    outputFile: {
+      'vitest-sonar-reporter': 'target/test-results/jest/TESTS-results-sonar.xml',
+    },
+    globals: true,
+    logHeapUsage: true,
+    minThreads: 1,
+    maxThreads: 2,
+    environment: 'jsdom',
+    cache: false,
+    include: ['src/test/javascript/spec/**/*.(spec|test).(ts|tsx)'],
+    exclude: ['node_modules', 'src/test/javascript/integration/**/*.spec.ts'],
+    coverage: {
+      clean: true,
+      provider: 'istanbul',
+      reportsDirectory: 'target/test-results/',
+      reporter: ['html', 'json-summary', 'text', 'text-summary', 'lcov', 'clover'],
+      watermarks: {
+        statements: [100, 100],
+        branches: [100, 100],
+        functions: [100, 100],
+        lines: [100, 100],
+      },
+    },
+  },
+});

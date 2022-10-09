0b338af086bd0de47cf3eb674afcf7e50f6c38c1
Quentin Monmert <quentin.monmert@gmail.com>
2022-10-09 19:07:52 +0200

    Svelte: move to Vitest

---
+++ src/main/resources/generator/client/svelte/vitest.config.ts
@@ -0,0 +1,41 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from 'path';
+
+export default defineConfig({
+  plugins: [svelte()],
+  resolve: {
+    alias: [
+      { find: '@webapp', replacement: path.resolve(__dirname, 'src/main/webapp') },
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/main/webapp/assets') },
+    ],
+  },
+  test: {
+    reporters: ['json', 'verbose', 'vitest-sonar-reporter'],
+    outputFile: {
+      'vitest-sonar-reporter': 'target/test-results/jest/TESTS-results-sonar.xml',
+    },
+    globals: true,
+    logHeapUsage: true,
+    minThreads: 1,
+    maxThreads: 2,
+    environment: 'jsdom',
+    cache: false,
+    include: ['src/test/javascript/spec/**/*.(spec|test).(ts|tsx)'],
+    exclude: ['node_modules'],
+    coverage: {
+      clean: true,
+      provider: 'istanbul',
+      reportsDirectory: 'target/test-results/',
+      reporter: ['html', 'json-summary', 'text', 'text-summary', 'lcov', 'clover'],
+      watermarks: {
+        statements: [100, 100],
+        branches: [100, 100],
+        functions: [100, 100],
+        lines: [100, 100],
+      },
+    },
+  },
+});

2aaaae6c33e19980b1b0c2ac5f59269a8ac47ad0
Quentin Monmert <quentin.monmert@gmail.com>
2022-10-10 11:28:22 +0200

    React: Vitest + Coverage

--- src/main/resources/generator/client/react/vitest.config.ts
+++ src/main/resources/generator/client/react/vitest.config.ts
@@ -23,6 +23,13 @@ export default defineConfig({
     include: ['src/test/javascript/spec/**/*.(spec|test).(ts|tsx)'],
     exclude: ['node_modules', 'src/test/javascript/integration/**/*.spec.ts'],
     coverage: {
+      all: true,
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      include: ['src/main/webapp/app/**/*.ts', 'src/main/webapp/app/**/*.tsx'],
+      exclude: ['src/main/webapp/app/index.tsx', 'src/main/webapp/app/**/*.d.ts'],
       clean: true,
       provider: 'istanbul',
       reportsDirectory: 'target/test-results/',

92d00ef3c09c7de0f7eb71439f8c3fe553a2a2c9
Frederik Hahne <atomfrede@gmail.com>
2022-11-09 22:19:47 +0100

    exclude all native hints from coverage

--- src/main/java/tech/jhipster/lite/project/infrastructure/secondary/NativeHints.java
+++ src/main/java/tech/jhipster/lite/project/infrastructure/secondary/NativeHints.java
@@ -3,9 +3,11 @@ package tech.jhipster.lite.project.infrastructure.secondary;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import tech.jhipster.lite.common.domain.Generated;
 import tech.jhipster.lite.project.domain.history.ProjectAction;
 import tech.jhipster.lite.project.domain.history.ProjectHistory;
 
+@Generated(reason = "Not testing native runtime hints")
 class NativeHints implements RuntimeHintsRegistrar {
 
   @Override
@@ -13,8 +15,8 @@ class NativeHints implements RuntimeHintsRegistrar {
     hints
       .reflection()
       .registerType(PersistedProjectHistory.class, MemberCategory.values())
-      .registerType(ProjectHistory.class, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS, MemberCategory.INTROSPECT_DECLARED_METHODS)
-      .registerType(ProjectAction.class, MemberCategory.INTROSPECT_DECLARED_CONSTRUCTORS, MemberCategory.INTROSPECT_DECLARED_METHODS)
+      .registerType(ProjectHistory.class, MemberCategory.values())
+      .registerType(ProjectAction.class, MemberCategory.values())
       .registerType(PersistedProjectAction.class, MemberCategory.values());
   }
 }

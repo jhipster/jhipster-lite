c8ec106c34425827443b35a2743df6b1f6e2f3f8
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-11-11 09:55:12 +0100

    Kafka: clean unused classes

--- src/main/java/tech/jhipster/lite/generator/server/springboot/broker/kafka/domain/Kafka.java
+++
@@ -1,9 +0,0 @@
-package tech.jhipster.lite.generator.server.springboot.broker.kafka.domain;
-
-public class Kafka {
-
-  public static final String KAFKA_DOCKER_IMAGE = "confluentinc/cp-kafka";
-  public static final String KAFKA_DOCKER_COMPOSE_FILE = "kafka.yml";
-
-  private Kafka() {}
-}

675e04fa14f4db6c124fbb814dc58715e05cc632
Quentin Monmert <quentin.monmert@gmail.com>
2023-02-04 16:38:38 +0100

    Angular 15: interceptor as a function

--- src/test/resources/projects/angular/main.ts
+++ src/test/resources/projects/angular/main.ts
@@ -1,4 +1,4 @@
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';

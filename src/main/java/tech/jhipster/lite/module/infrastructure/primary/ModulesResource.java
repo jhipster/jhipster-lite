5b6b7ffba27da6d07d8c74d00ad31141433ddeea
Hippolyte Durix <hdurix@ippon.fr>
2023-06-23 11:42:01 +0200

    Remove forced Sonar errors

--- src/main/java/tech/jhipster/lite/module/infrastructure/primary/ModulesResource.java
+++ src/main/java/tech/jhipster/lite/module/infrastructure/primary/ModulesResource.java
@@ -52,7 +52,6 @@ class ModulesResource {
   @PostMapping("apply-patches")
   @Operation(summary = "Apply multiple modules patches")
   public void applyPatches(@RequestBody @Validated RestJHipsterModulesToApply modulesToApply) {
-    String useless = "";
     modules.apply(modulesToApply.toDomain(projectFolder));
   }
 

5baf26270060706cd6873282577998e5973734bc
Quentin <quentin.monmert@gmail.com>
2023-07-12 19:13:54 +0200

    Add config files to prettier processing

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,6 @@ module.exports = {
   '{src/test/javascript,src/main/webapp}/**/*.{js,ts,tsx,vue}': ['eslint --fix'],
   'src/main/style/**/*.{css,scss}': ['stylelint --fix'],
   'src/main/style/**/*.pug': ['pug-lint'],
-  '{{src/**/,}*.{md,json,yml,html,vue,java,xml},*.{js,ts},src/{main/webapp,main/glyph,test/javascript}/**/*.{css,scss}}': [
-    'prettier --write',
-  ],
+  '{{src/**/,}*.{md,json,yml,html,vue,java,xml},*.{js,ts},.github/**/*.yml,documentation/**/*.md,src/{main/webapp,main/glyph,test/javascript}/**/*.{css,scss}}':
+    ['prettier --write'],
 };

e1acca8653ff8b7158fcccf1844660f5e4dba8c2
abdelfetah18 <nstart0319@gmail.com>
2023-07-14 22:24:25 +0100

    Simplify the code and add return value type

--- src/main/webapp/app/module/primary/landscape/LandscapeNavigation.ts
+++ src/main/webapp/app/module/primary/landscape/LandscapeNavigation.ts
@@ -1,5 +1,8 @@
 import { ModuleSlug } from '@/module/domain/ModuleSlug';
+import { LandscapeElement } from '@/module/domain/landscape/LandscapeElement';
+import { LandscapeElementId } from '@/module/domain/landscape/LandscapeElementId';
 import { LandscapeLevel } from '@/module/domain/landscape/LandscapeLevel';
+import { LandscapeModule } from '@/module/domain/landscape/LandscapeModule';
 
 export class LandscapeNavigation {
   private currentModule: number;
@@ -16,41 +19,41 @@ export class LandscapeNavigation {
     this.levels = levels;
   }
 
-  private level() {
+  private level(): LandscapeLevel {
     return this.levels[this.currentLevel];
   }
 
-  private element() {
+  private element(): LandscapeElement {
     return this.level().elements[this.currentElement];
   }
 
-  private module() {
+  private module(): LandscapeModule {
     return this.element().allModules()[this.currentModule];
   }
 
-  public goUp() {
+  public goUp(): void {
     this.nextElement(true);
   }
 
-  public goDown() {
+  public goDown(): void {
     this.nextElement(false);
   }
 
-  public goLeft() {
+  public goLeft(): void {
     const current_module = this.module().slug();
     this.currentLevel = this.decrease(this.currentLevel);
 
     this.calculateDistanceAndUpdate(current_module);
   }
 
-  public goRight() {
+  public goRight(): void {
     const current_module = this.module().slug();
 
     this.currentLevel = this.increase(this.currentLevel, this.levels.length - 1);
     this.calculateDistanceAndUpdate(current_module);
   }
 
-  public goToDependencie() {
+  public goToDependencie(): void {
     if (this.module().dependencies().length == 0) {
       return;
     }
@@ -61,20 +64,22 @@ export class LandscapeNavigation {
     this.updateCursor(this.currentLevel - 1, element_index, 0);
   }
 
-  public goToDependent() {
-    const next_level = this.levels[this.currentLevel + 1];
-    const element_index = next_level.elements.findIndex(element => {
-      return (
-        element.allModules().findIndex(module => {
-          return (
-            module.dependencies().findIndex(e => {
-              return e.get() == this.element().slug().get();
-            }) >= 0
-          );
-        }) >= 0
-      );
-    });
+  public findDependency(dependencies: LandscapeElementId[]): number {
+    return dependencies.findIndex(module => module.get() === this.module().slug().get());
+  }
 
+  public findDependentModuleIndex(modules: LandscapeModule[]): number {
+    return modules.findIndex(module => this.findDependency(module.dependencies()) >= 0);
+  }
+
+  public findElementIndexOfDependentModule(elements: LandscapeElement[]): number {
+    return elements.findIndex(element => this.findDependentModuleIndex(element.allModules()) >= 0);
+  }
+
+  public goToDependent(): void {
+    const next_level = this.levels[this.currentLevel + 1];
+
+    const element_index = this.findElementIndexOfDependentModule(next_level.elements);
     if (element_index == -1) {
       return;
     }
@@ -82,20 +87,17 @@ export class LandscapeNavigation {
     this.updateCursor(this.currentLevel + 1, element_index, 0);
   }
 
-  public getSlug() {
+  public getSlug(): ModuleSlug {
     return this.module().slug();
   }
 
-  private calculateDistanceAndUpdate(current_module: ModuleSlug) {
+  private calculateDistanceAndUpdate(current_module: ModuleSlug): void {
     const current_module_html = this.landscapeElements.get(current_module.get()) as HTMLElement;
 
-    // calculateDistanceAndUpdate
     let smallest_distance = Infinity;
-    // Iterate throw Element and Modules until you get the smallest one.
     this.level().elements.forEach((element, element_index) => {
       element.allModules().forEach((module, index) => {
         const module_html_element = this.landscapeElements.get(module.slug().get()) as HTMLElement;
-        // Compare the distance from the currentModule to That Module.
         const distance = Math.abs(module_html_element.getBoundingClientRect().y - current_module_html.getBoundingClientRect().y);
         if (distance < smallest_distance) {
           smallest_distance = distance;
@@ -105,15 +107,15 @@ export class LandscapeNavigation {
     });
   }
 
-  private increase(value: number, max: number) {
+  private increase(value: number, max: number): number {
     return value < max ? value + 1 : max;
   }
 
-  private decrease(value: number) {
+  private decrease(value: number): number {
     return value > 0 ? value - 1 : 0;
   }
 
-  private nextModule(ArrowUp: boolean) {
+  private nextModule(ArrowUp: boolean): boolean {
     const old_value = this.currentModule;
     if (ArrowUp) {
       this.currentModule = this.decrease(this.currentModule);
@@ -124,7 +126,7 @@ export class LandscapeNavigation {
     return this.currentModule != old_value;
   }
 
-  private nextElement(ArrowUp: boolean) {
+  private nextElement(ArrowUp: boolean): void {
     if (this.nextModule(ArrowUp)) {
       return;
     }
@@ -138,7 +140,7 @@ export class LandscapeNavigation {
     }
   }
 
-  private updateCursor(currentLevel: number, currentElement: number, currentModule: number) {
+  private updateCursor(currentLevel: number, currentElement: number, currentModule: number): void {
     this.currentLevel = currentLevel;
     this.currentElement = currentElement;
     this.currentModule = currentModule;

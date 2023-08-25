775e861c34fafd85b8d0f425a1368e12a8630ca1
Aurélien Mino <aurelien.mino@gmail.com>
2023-08-25 09:08:48 +0200

    generator: Run Cucumber tests with JUnit5

--- src/test/java/tech/jhipster/lite/cucumber/CucumberTest.java
+++ src/test/java/tech/jhipster/lite/cucumber/CucumberTest.java
@@ -1,17 +1,20 @@
 package tech.jhipster.lite.cucumber;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
 import tech.jhipster.lite.ComponentTest;
 
+@Suite
 @ComponentTest
-@RunWith(Cucumber.class)
-@CucumberOptions(
-  glue = "tech.jhipster.lite",
-  plugin = {
-    "pretty", "json:target/cucumber/cucumber.json", "html:target/cucumber/cucumber.htm", "junit:target/cucumber/TEST-cucumber.xml",
-  },
-  features = "src/test/features"
+@IncludeEngines("cucumber")
+@SuppressWarnings("java:S2187")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "tech.jhipster.lite")
+@ConfigurationParameter(
+  key = PLUGIN_PROPERTY_NAME,
+  value = "pretty, json:target/cucumber/cucumber.json, html:target/cucumber/cucumber.htm, junit:target/cucumber/TEST-cucumber.xml"
 )
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/features")
 public class CucumberTest {}

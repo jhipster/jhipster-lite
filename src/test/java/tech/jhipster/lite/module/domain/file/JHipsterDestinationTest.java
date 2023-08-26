1c17096f04ee14c3dc5090fe0edf2b5aa6cfdf31
Quentin <quentin.monmert@gmail.com>
2023-08-26 10:47:23 +0200

    Fix typo: Happning=>Happening

--- src/test/java/tech/jhipster/lite/module/domain/file/JHipsterDestinationTest.java
+++ src/test/java/tech/jhipster/lite/module/domain/file/JHipsterDestinationTest.java
@@ -18,7 +18,7 @@ class JHipsterDestinationTest {
   private static final JHipsterProjectFolder PROJECT = new JHipsterProjectFolder(TestFileUtils.tmpDirForTest());
 
   @Test
-  void shouldAddSlashWhenHappningElementithoutSlash() {
+  void shouldAddSlashWhenHappeningElementithoutSlash() {
     assertThat(new JHipsterDestination("src/main").append("file").pathInProject(PROJECT).toString()).endsWith(path("src", "main", "file"));
   }
 

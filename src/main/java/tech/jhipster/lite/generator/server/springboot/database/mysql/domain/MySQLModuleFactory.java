cf41ded8bc351f8180b030a19ef6ae9cd81166a9
Quentin <quentin.monmert@gmail.com>
2023-11-01 15:06:24 +0100

    Rename MYSQL_GROUPID=>MYSQL_GROUP_ID

--- src/main/java/tech/jhipster/lite/generator/server/springboot/database/mysql/domain/MySQLModuleFactory.java
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/database/mysql/domain/MySQLModuleFactory.java
@@ -13,7 +13,7 @@ import tech.jhipster.lite.shared.error.domain.Assert;
 public class MySQLModuleFactory {
 
   private static final String MYSQL = "mysql";
-  private static final String MYSQL_GROUPID = "com.mysql";
+  private static final String MYSQL_GROUP_ID = "com.mysql";
   private static final String MYSQL_ARTIFACTID = "mysql-connector-j";
 
   private final DockerImages dockerImages;
@@ -29,7 +29,7 @@ public class MySQLModuleFactory {
 
     return sqlCommonModuleBuilder(properties, DatabaseType.MYSQL, dockerImages.get(MYSQL), documentationTitle("MySQL"), artifactId(MYSQL))
       .javaDependencies()
-      .addDependency(javaDependency().groupId(MYSQL_GROUPID).artifactId(MYSQL_ARTIFACTID).scope(JavaDependencyScope.RUNTIME).build())
+      .addDependency(javaDependency().groupId(MYSQL_GROUP_ID).artifactId(MYSQL_ARTIFACTID).scope(JavaDependencyScope.RUNTIME).build())
       .and()
       .springMainProperties()
       .set(propertyKey("spring.datasource.url"), propertyValue("jdbc:mysql://localhost:3306/" + properties.projectBaseName().name()))

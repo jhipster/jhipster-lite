4eae5cbd36a3bec274542389055a19f037332017
Quentin <quentin.monmert@gmail.com>
2023-11-15 13:11:35 +0100

    Fix: Upgrade sonarqube 10.3.0-community

--- src/test/resources/projects/react-app/App.tsx
+++ src/test/resources/projects/react-app/App.tsx
@@ -12,25 +12,21 @@ function App() {
         <img alt="JHipster logo" src={JHipsterLiteNeonBlue} />
         <h1>React + TypeScript + Vite</h1>
         <p>
-          Recommended IDE setup:&nbsp;
-          <a href="https://code.visualstudio.com/" target="_blank">
-            VSCode
-          </a>
+          Recommended IDE setup:&nbsp;<a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
         </p>
 
         <p>
           <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">
             Vite Documentation
           </a>
-          &nbsp;|&nbsp;
+          <span>&nbsp;|&nbsp;</span>
           <a href="https://reactjs.org/docs/" target="_blank">
             React Documentation
           </a>
         </p>
 
         <p>
-          Edit&nbsp;
-          <code>src/main/webapp/app/common/primary/app/App.tsx</code> to test hot module replacement.
+          Edit&nbsp;<code>src/main/webapp/app/common/primary/app/App.tsx</code> to test hot module replacement.
         </p>
       </div>
     </div>

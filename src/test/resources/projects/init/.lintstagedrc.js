149ce87d5dc042f0df32126f3fcf51089cf2eff5
Aurélien Mino <aurelien.mino@gmail.com>
2023-12-17 21:09:41 +0100

    fix tests

--- src/test/resources/projects/init/.lintstagedrc.js
+++ src/test/resources/projects/init/.lintstagedrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  '{src/**/,}*.{md,json,yml,html,css,scss,java,xml}': ['prettier --write'],
+  '{src/**/,}*.{md,json,yml,html,css,scss,java,xml,feature}': ['prettier --write'],
 };

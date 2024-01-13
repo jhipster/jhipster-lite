1a500e8fadc80467c3a647679683539cd50dc245
Aurélien Mino <aurelien.mino@gmail.com>
2024-01-13 14:56:31 +0100

    ProtobufBackwardsCompatibilityCheckModule currently only works with maven, so add a dependency to Maven slug to make it explicit

--- src/main/java/tech/jhipster/lite/generator/server/javatool/protobuf/infrastructure/primary/ProtobufModuleConfiguration.java
+++ src/main/java/tech/jhipster/lite/generator/server/javatool/protobuf/infrastructure/primary/ProtobufModuleConfiguration.java
@@ -31,7 +31,9 @@ class ProtobufModuleConfiguration {
       .slug(JHLiteModuleSlug.PROTOBUF_BACKWARDS_COMPATIBILITY_CHECK)
       .propertiesDefinition(JHipsterModulePropertiesDefinition.builder().addBasePackage().addIndentation().build())
       .apiDoc("Java", "Add protobuf backwards compatibility check")
-      .organization(JHipsterModuleOrganization.builder().addDependency(JHLiteModuleSlug.PROTOBUF).build())
+      .organization(
+        JHipsterModuleOrganization.builder().addDependency(JHLiteModuleSlug.PROTOBUF).addDependency(JHLiteModuleSlug.MAVEN_JAVA).build()
+      )
       .tags("server", "protobuf")
       .factory(protobuf::buildProtobufBackwardsCompatibilityCheckModule);
   }

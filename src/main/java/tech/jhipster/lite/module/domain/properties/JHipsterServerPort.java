65ea87bdc6472681be0f8947e9b6d8c5c9bd94d2
Zsombor Gegesy <gzsombor@gmail.com>
2024-01-19 21:59:39 +0100

    Convert JHipsterServerPort to a record

--- src/main/java/tech/jhipster/lite/module/domain/properties/JHipsterServerPort.java
+++ src/main/java/tech/jhipster/lite/module/domain/properties/JHipsterServerPort.java
@@ -2,19 +2,18 @@ package tech.jhipster.lite.module.domain.properties;
 
 import tech.jhipster.lite.shared.error.domain.Assert;
 
-public class JHipsterServerPort {
-
+public record JHipsterServerPort(int serverPort) {
   private static final int DEFAULT_PORT = 8080;
 
-  private final int serverPort;
-
-  public JHipsterServerPort(Integer port) {
-    serverPort = buildPort(port);
-
+  public JHipsterServerPort {
     Assert.field("serverPort", serverPort).min(1).max(65_535);
   }
 
-  private int buildPort(Integer port) {
+  public JHipsterServerPort(Integer port) {
+    this(buildPort(port));
+  }
+
+  private static int buildPort(Integer port) {
     if (port == null) {
       return DEFAULT_PORT;
     }

54d51da2ce4f71acf810ec8db5c81d07456bf3b4
Zsombor Gegesy <gzsombor@gmail.com>
2024-01-20 11:24:31 +0100

    Add test for the bound check

--- src/test/java/tech/jhipster/lite/module/domain/properties/JHipsterServerPortTest.java
+++ src/test/java/tech/jhipster/lite/module/domain/properties/JHipsterServerPortTest.java
@@ -1,9 +1,12 @@
 package tech.jhipster.lite.module.domain.properties;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import tech.jhipster.lite.UnitTest;
+import tech.jhipster.lite.shared.error.domain.NumberValueTooHighException;
+import tech.jhipster.lite.shared.error.domain.NumberValueTooLowException;
 
 @UnitTest
 class JHipsterServerPortTest {
@@ -16,6 +19,12 @@ class JHipsterServerPortTest {
   @Test
   void shouldGetServerPortFromPort() {
     assertThat(new JHipsterServerPort(9000).get()).isEqualTo(9000);
+  }
+
+  @Test
+  void shouldValidatePortNumbers() {
+    assertThrows(NumberValueTooHighException.class, () -> new JHipsterServerPort(90000));
+    assertThrows(NumberValueTooLowException.class, () -> new JHipsterServerPort(-90000));
   }
 
   @Test

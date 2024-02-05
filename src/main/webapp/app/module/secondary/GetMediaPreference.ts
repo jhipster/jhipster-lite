d22b3946e30726ef5a3de9ce7b189a9763563d27
Quentin <quentin.monmert@gmail.com>
2024-02-05 20:58:53 +0100

    Sonar: Prefer using an optional chain expression

--- src/main/webapp/app/module/secondary/GetMediaPreference.ts
+++ src/main/webapp/app/module/secondary/GetMediaPreference.ts
@@ -1,7 +1,7 @@
 export type ThemePreference = 'light-theme' | 'dark-theme';
 
 export const getMediaPreference = (win: Window): ThemePreference => {
-  if (typeof win !== 'undefined' && win.matchMedia) {
+  if (win?.matchMedia) {
     return win.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark-theme' : 'light-theme';
   }
   return 'light-theme';

fdc682be9c4e0ef6f6d472d5e7f9982641f08b58
Aurélien Mino <aurelien.mino@gmail.com>
2024-02-26 23:41:36 +0100

    (native-hints) Register TomlFormat Fixes #8789

---
+++ src/main/java/tech/jhipster/lite/module/infrastructure/secondary/javadependency/gradle/NativeGradleConfiguration.java
@@ -0,0 +1,10 @@
+package tech.jhipster.lite.module.infrastructure.secondary.javadependency.gradle;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
+import tech.jhipster.lite.shared.generation.domain.ExcludeFromGeneratedCodeCoverage;
+
+@ImportRuntimeHints(NativeHints.class)
+@ExcludeFromGeneratedCodeCoverage(reason = "Not testing native configuration")
+@Configuration
+public class NativeGradleConfiguration {}

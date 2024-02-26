b936139d7e8d7f088e2c4e3b45a1bd00a63af22a
Aurélien Mino <aurelien.mino@gmail.com>
2024-02-27 00:00:56 +0100

    (ci) Test native image by starting a container and verifying health endpoint

---
+++ tests-ci/start_docker_container.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+show_syntax() {
+  echo "Usage: $0 <docker-image>" >&2
+  exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+  show_syntax
+fi
+
+DOCKER_IMAGE=$1
+PORT=7471
+
+# Start docker container
+echo "*** Starting docker container using image ${DOCKER_IMAGE}..."
+docker run -p $PORT:7471 --rm -d ${DOCKER_IMAGE}
+
+retryCount=1
+maxRetry=30
+httpUrl="http://localhost:"$PORT"/management/health"
+
+rep=$(curl -v "$httpUrl")
+status=$?
+while [ "$status" -ne 0 ] && [ "$retryCount" -le "$maxRetry" ]; do
+  echo "*** [$(date)] Application not reachable yet. Sleep and retry - retryCount =" $retryCount "/" $maxRetry
+  retryCount=$((retryCount+1))
+  sleep 5
+  rep=$(curl -v "$httpUrl")
+  status=$?
+done
+
+if [ "$status" -ne 0 ]; then
+  echo "*** [$(date)] Not connected after" $retryCount " retries."
+  return 1
+fi

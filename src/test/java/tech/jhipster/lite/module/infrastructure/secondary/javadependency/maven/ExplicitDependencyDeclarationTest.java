62257b11fe4127f30d6791d7ccc78f3f259592ed
Aurélien Mino <aurelien.mino@gmail.com>
2024-02-27 14:23:10 +0100

    Test to check any unused dependency in maven current dependencies source + cleanup

---
+++ src/test/java/tech/jhipster/lite/module/infrastructure/secondary/javadependency/maven/ExplicitDependencyDeclarationTest.java
@@ -0,0 +1,38 @@
+package tech.jhipster.lite.module.infrastructure.secondary.javadependency.maven;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.fabric8.maven.Maven;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.stream.Stream;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import tech.jhipster.lite.UnitTest;
+
+@UnitTest
+class ExplicitDependencyDeclarationTest {
+
+  private static final String CURRENT_VERSIONS_FILE = "/generator/dependencies/pom.xml";
+
+  public static Stream<Arguments> dependencies() {
+    Model mavenModel = readMavenModel();
+    return mavenModel.getDependencyManagement().getDependencies().stream().map(Arguments::of);
+  }
+
+  @MethodSource("dependencies")
+  @ParameterizedTest
+  void allDeclaredDependenciesShouldUseAVersion(Dependency dependency) {
+    assertThat(dependency.getVersion()).isNotBlank();
+  }
+
+  private static Model readMavenModel() {
+    var reader = new InputStreamReader(
+      Objects.requireNonNull(ExplicitDependencyDeclarationTest.class.getResourceAsStream(CURRENT_VERSIONS_FILE))
+    );
+    return Maven.readModel(reader);
+  }
+}

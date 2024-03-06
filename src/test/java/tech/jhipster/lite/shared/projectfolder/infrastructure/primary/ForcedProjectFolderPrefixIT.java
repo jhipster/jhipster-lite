8bb94762ce4042c8b115d5550af90a90d495fefd
Quentin <quentin.monmert@gmail.com>
2024-03-06 21:34:44 +0100

    Unnecessary braces

--- src/test/java/tech/jhipster/lite/shared/projectfolder/infrastructure/primary/ForcedProjectFolderPrefixIT.java
+++ src/test/java/tech/jhipster/lite/shared/projectfolder/infrastructure/primary/ForcedProjectFolderPrefixIT.java
@@ -13,7 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import tech.jhipster.lite.IntegrationTest;
 
 @AutoConfigureMockMvc
-@IntegrationTest(properties = { "application.forced-project-folder=/tmp/forced" })
+@IntegrationTest(properties = "application.forced-project-folder=/tmp/forced")
 class ForcedProjectFolderPrefixIT {
 
   private static final String SEPARATOR = FileSystems.getDefault().getSeparator();

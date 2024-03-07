d8ab5b0c39075ae9db76c38966d00ce571527e35
Aurélien Mino <aurelien.mino@gmail.com>
2024-03-07 14:16:49 +0100

    Remove unused imports

--- src/test/java/tech/jhipster/lite/HexagonalArchTest.java
+++ src/test/java/tech/jhipster/lite/HexagonalArchTest.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 @UnitTest
 class HexagonalArchTest {

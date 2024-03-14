98909886465619c471f71e8c234c5d98ee6e6216
Renan Meneses de Andrade Franca <renan.andradefranca@gmail.com>
2024-03-14 07:42:20 -0300

    improve error messages

--- src/main/java/tech/jhipster/lite/module/infrastructure/secondary/javadependency/gradle/MissingGradleProfileException.java
+++ src/main/java/tech/jhipster/lite/module/infrastructure/secondary/javadependency/gradle/MissingGradleProfileException.java
@@ -8,7 +8,7 @@ class MissingGradleProfileException extends GeneratorException {
   public MissingGradleProfileException(BuildProfileId profileId) {
     super(
       badRequest(GradleDependencyErrorKey.MISSING_PROFILE).message(
-        "Your gradle project does not contain the profile-%s.gradle.kts file".formatted(profileId)
+        "Your gradle project does not contain any profile-%s.gradle.kts precompiled script plugin".formatted(profileId)
       )
     );
   }

ea0c22cfe2a165bbc1cd26acd710a7f5b64d45a0
Aurélien Mino <aurelien.mino@gmail.com>
2024-04-20 10:23:50 +0200

    chore(lint-staged): adjust configuration to apply prettier more globally

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,5 +3,5 @@ export default {
   'src/main/style/**/*.{css,scss}': ['stylelint --fix', 'prettier --write'],
   'src/main/glyph/**/*.{css,scss}': ['prettier --write'],
   'src/main/style/**/*.pug': ['eslint --fix'],
-  '{{src/**/,}*.{md,json,yml,html,java,xml,feature},*.{js,cjs,ts},.github/**/*.yml,documentation/**/*.md}': ['prettier --write'],
+  '{{**/,}*.{md,json,yml,html,java,xml,feature},*.{js,cjs,ts}}': ['prettier --write'],
 };

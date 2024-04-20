20c28e8b65680db38d4333369cd985d08f38fcaf
Aurélien Mino <aurelien.mino@gmail.com>
2024-04-20 09:35:25 +0200

    fix(lint-staged): adjust configuration to re-add js and ts files on root directory

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,5 @@ export default {
   '{src/test/javascript,src/main/webapp}/**/*.{js,ts,tsx,vue}': ['eslint --fix', 'prettier --write'],
   'src/main/style/**/*.{css,scss}': ['stylelint --fix', 'prettier --write'],
   'src/main/style/**/*.pug': ['eslint --fix'],
-  '{{src/**/,}*.{md,json,yml,html,java,xml,feature},.github/**/*.yml,documentation/**/*.md}':
-    ['prettier --write'],
+  '{{src/**/,}*.{md,json,yml,html,java,xml,feature},*.{js,ts},.github/**/*.yml,documentation/**/*.md}': ['prettier --write'],
 };

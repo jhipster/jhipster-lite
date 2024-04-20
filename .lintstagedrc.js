fa3c9fa8a29f58eff94e6d036490e1038985cb95
Aurélien Mino <aurelien.mino@gmail.com>
2024-04-20 09:36:07 +0200

    fix(lint-staged): adjust configuration to re-add style in src/main/glyph

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,7 @@
 export default {
   '{src/test/javascript,src/main/webapp}/**/*.{js,ts,tsx,vue}': ['eslint --fix', 'prettier --write'],
   'src/main/style/**/*.{css,scss}': ['stylelint --fix', 'prettier --write'],
+  'src/main/glyph/**/*.{css,scss}': ['prettier --write'],
   'src/main/style/**/*.pug': ['eslint --fix'],
   '{{src/**/,}*.{md,json,yml,html,java,xml,feature},*.{js,ts},.github/**/*.yml,documentation/**/*.md}': ['prettier --write'],
 };

e7902cce6ca7cc0ff8a92257c72e7b3c2e63ffcb
Aurélien Mino <aurelien.mino@gmail.com>
2024-05-03 21:52:58 +0200

    Use gitignore API for angular module

--- src/main/java/tech/jhipster/lite/generator/client/angular/core/domain/AngularModuleFactory.java
+++ src/main/java/tech/jhipster/lite/generator/client/angular/core/domain/AngularModuleFactory.java
@@ -22,6 +22,11 @@ public class AngularModuleFactory {
   public JHipsterModule buildModule(JHipsterModuleProperties properties) {
     //@formatter:off
     return ClientsModulesFactory.clientModuleBuilder(properties)
+      .git()
+        .ignoreComment("Angular")
+        .ignorePattern(".angular/")
+        .ignorePattern(".nx/")
+        .and()
       .packageJson()
         .addDependency(packageName("@angular/animations"), ANGULAR, ANGULAR_CORE_PACKAGE)
         .addDependency(packageName("@angular/cdk"), ANGULAR)

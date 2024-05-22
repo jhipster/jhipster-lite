2b685b29eaeadb4bfe597829df64d1a6ca545599
Aurélien Mino <aurelien.mino@gmail.com>
2024-05-22 14:34:19 +0200

    Simplify lintstaged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,8 @@
 export default {
-  '{src/test/javascript,src/main/webapp}/**/*.{js,ts,tsx,vue}': ['eslint --fix'],
+  'src/{test/javascript,main/webapp}/**/*.{ts,vue}': ['eslint --fix'],
+  '{,src/*/resources/**/}*.{js,cjs,ts,tsx,vue}': ['prettier --write'],
   'src/main/style/**/*.{css,scss}': ['stylelint --fix', 'prettier --write'],
   'src/main/glyph/**/*.{css,scss}': ['prettier --write'],
-  'src/main/style/**/*.pug': ['eslint --fix', 'prettier --write'],
-  '{{**/,}*.{md,json,yml,html,java,xml,feature},*.{js,cjs,ts}}': ['prettier --write'],
+  '*.pug': ['eslint --fix', 'prettier --write'],
+  '*.{md,json,yml,html,java,xml,feature}': ['prettier --write'],
 };

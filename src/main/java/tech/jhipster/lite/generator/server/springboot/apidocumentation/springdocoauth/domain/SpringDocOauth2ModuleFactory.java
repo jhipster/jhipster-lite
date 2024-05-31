7da9ec8f8d0b915b4fded53ff8156d78f70d1e85
Pascal Grimaud <pascalgrimaud@gmail.com>
2024-05-31 18:52:07 +0200

    SpringDoc: fix realm name with OAuth2

--- src/main/java/tech/jhipster/lite/generator/server/springboot/apidocumentation/springdocoauth/domain/SpringDocOauth2ModuleFactory.java
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/apidocumentation/springdocoauth/domain/SpringDocOauth2ModuleFactory.java
@@ -2,6 +2,7 @@ package tech.jhipster.lite.generator.server.springboot.apidocumentation.springdo
 
 import static tech.jhipster.lite.module.domain.JHipsterModule.*;
 
+import java.util.regex.Pattern;
 import tech.jhipster.lite.module.domain.JHipsterModule;
 import tech.jhipster.lite.module.domain.file.JHipsterSource;
 import tech.jhipster.lite.module.domain.properties.JHipsterModuleProperties;
@@ -9,10 +10,17 @@ import tech.jhipster.lite.shared.error.domain.Assert;
 
 public class SpringDocOauth2ModuleFactory {
 
+  public static final String REALM_NAME = "keycloakRealmName";
+  public static final String DEFAULT_REALM_NAME = "jhipster";
+  private static final Pattern NAME_FORMAT = Pattern.compile("^[a-z0-9-]+$");
+
   private static final JHipsterSource SOURCE = from("server/springboot/apidocumentation/springdocoauth");
 
   public JHipsterModule buildModule(JHipsterModuleProperties properties) {
     Assert.notNull("properties", properties);
+
+    String realmName = properties.getOrDefaultString(REALM_NAME, DEFAULT_REALM_NAME);
+    Assert.field(REALM_NAME, realmName).notNull().matchesPattern(NAME_FORMAT).maxLength(30);
 
     //@formatter:off
     return moduleBuilder(properties)
@@ -26,18 +34,18 @@ public class SpringDocOauth2ModuleFactory {
         .and()
       .springMainProperties()
         .set(propertyKey("springdoc.swagger-ui.oauth.client-id"), propertyValue("web_app"))
-        .set(propertyKey("springdoc.swagger-ui.oauth.realm"), propertyValue("jhipster"))
+        .set(propertyKey("springdoc.swagger-ui.oauth.realm"), propertyValue(realmName))
         .set(
           propertyKey("springdoc.oauth2.authorization-url"),
-          propertyValue("http://localhost:9080/realms/jhipster/protocol/openid-connect/auth")
+          propertyValue("http://localhost:9080/realms/" + realmName + "/protocol/openid-connect/auth")
         )
         .and()
       .springTestProperties()
         .set(propertyKey("springdoc.swagger-ui.oauth.client-id"), propertyValue("web_app"))
-        .set(propertyKey("springdoc.swagger-ui.oauth.realm"), propertyValue("jhipster"))
+        .set(propertyKey("springdoc.swagger-ui.oauth.realm"), propertyValue(realmName))
         .set(
           propertyKey("springdoc.oauth2.authorization-url"),
-          propertyValue("http://localhost:9080/realms/jhipster/protocol/openid-connect/auth")
+          propertyValue("http://localhost:9080/realms/" + realmName + "/protocol/openid-connect/auth")
         )
         .and()
       .build();

101afca72a34ac866661884009d2d70ed8b63768
Renan Franca <renan.andradefranca@gmail.com>
2024-08-10 15:28:06 -0300

    refactor(LandscapePresetConfigurationComponent.spec): remove unnecessary adjective from test name

--- src/test/webapp/unit/module/primary/landscape/LandscapePresetConfigurationComponent.spec.ts
+++ src/test/webapp/unit/module/primary/landscape/LandscapePresetConfigurationComponent.spec.ts
@@ -43,7 +43,7 @@ describe('LandscapePresetConfigurationComponent', () => {
       });
   });
 
-  it('should handle API error gracefully', async () => {
+  it('should handle API error', async () => {
     vi.spyOn(console, 'error').mockImplementation(() => {});
     const error = new Error('API Error');
     const modulesRepository = repositoryWithPresetError(error);

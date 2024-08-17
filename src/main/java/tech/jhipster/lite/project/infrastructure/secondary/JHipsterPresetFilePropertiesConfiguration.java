2a5cb7bc0d2fcc9ce849f4f0850d3b02c01f34cb
Renan Franca <renan.andradefranca@gmail.com>
2024-08-17 13:34:11 -0300

    fix(JHipsterPresetFilePropertiesConfiguration): add missing preset file name property injection parameter

---
+++ src/main/java/tech/jhipster/lite/project/infrastructure/secondary/JHipsterPresetFilePropertiesConfiguration.java
@@ -0,0 +1,16 @@
+package tech.jhipster.lite.project.infrastructure.secondary;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tech.jhipster.lite.project.domain.resource.JHipsterPresetFile;
+
+@Configuration
+@EnableConfigurationProperties(JHipsterPresetFileProperties.class)
+class JHipsterPresetFilePropertiesConfiguration {
+
+  @Bean
+  JHipsterPresetFile jhipsterPresetFile(JHipsterPresetFileProperties presetFileProperties) {
+    return new JHipsterPresetFile(presetFileProperties.getName());
+  }
+}

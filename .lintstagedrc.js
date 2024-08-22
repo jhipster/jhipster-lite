151391009fa393aa637d81df6b21377181244d86
Aurélien Mino <aurelien.mino@gmail.com>
2024-08-22 13:55:40 +0200

    chore(generator): simplify stylelint configuration and trigger, by declaration a .stylelintignore file

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,6 @@
 export default {
   '*.{js,cjs,ts,tsx,vue}': ['eslint --fix', 'prettier --write'],
-  'src/main/style/**/*.{css,scss}': ['stylelint --fix', 'prettier --write'],
-  'src/main/glyph/**/*.{css,scss}': ['prettier --write'],
+  '*.{css,scss}': ['stylelint --fix', 'prettier --write'],
   '*.pug': ['eslint --fix', 'prettier --write'],
   '*.{md,json,yml,html,java,xml,feature}': ['prettier --write'],
 };

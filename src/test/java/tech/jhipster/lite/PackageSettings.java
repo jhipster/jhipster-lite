5a6113906fb07710cea46536cd6bcffda2a61045
Aurélien Mino <aurelien.mino@gmail.com>
2024-09-16 22:32:27 +0200

    Apply module: approval-tests

---
+++ src/test/java/tech/jhipster/lite/PackageSettings.java
@@ -0,0 +1,9 @@
+package tech.jhipster.lite;
+
+/**
+ * ApprovalTests.Java configuration.
+ * */
+public class PackageSettings {
+
+  private static final String ApprovalBaseDirectory = "../resources";
+}

f3beaec082cddcf5ec538c363a36a04622863a0e
Quentin <quentin.monmert@gmail.com>
2024-09-22 16:30:21 +0200

    Generator: add missing test

---
+++ src/test/webapp/unit/shared/alert/infrastructure/primary/WindowApplicationListener.spec.ts
@@ -0,0 +1,24 @@
+import { WindowApplicationListener } from '@/shared/alert/infrastructure/primary/WindowApplicationListener';
+import { describe, it, expect, vi } from 'vitest';
+
+describe('WindowApplicationListener', () => {
+  it('should add event listener on window', () => {
+    vi.spyOn(window, 'addEventListener').mockImplementation(() => {});
+
+    const windowApplicationListener = new WindowApplicationListener(window);
+
+    windowApplicationListener.addEventListener('success', () => {});
+
+    expect(window.addEventListener).toHaveBeenCalledTimes(1);
+  });
+
+  it('should remove event listener on window', () => {
+    vi.spyOn(window, 'removeEventListener').mockImplementation(() => {});
+
+    const windowApplicationListener = new WindowApplicationListener(window);
+
+    windowApplicationListener.removeEventListener('success', () => {});
+
+    expect(window.removeEventListener).toHaveBeenCalledTimes(1);
+  });
+});

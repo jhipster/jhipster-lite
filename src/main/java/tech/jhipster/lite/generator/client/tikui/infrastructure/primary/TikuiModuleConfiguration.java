30e4cc63333bd693072ed49e710378f9b57c6c1d
Aurélien Mino <aurelien.mino@gmail.com>
2024-09-30 00:27:09 +0200

    fix: following rebase on main

--- src/main/java/tech/jhipster/lite/generator/client/tikui/infrastructure/primary/TikuiModuleConfiguration.java
+++ src/main/java/tech/jhipster/lite/generator/client/tikui/infrastructure/primary/TikuiModuleConfiguration.java
@@ -1,10 +1,11 @@
 package tech.jhipster.lite.generator.client.tikui.infrastructure.primary;
 
+import static tech.jhipster.lite.shared.slug.domain.JHLiteFeatureSlug.CLIENT_CORE;
+import static tech.jhipster.lite.shared.slug.domain.JHLiteModuleSlug.TIKUI;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tech.jhipster.lite.generator.client.tikui.application.TikuiApplicationService;
-import tech.jhipster.lite.generator.slug.domain.JHLiteFeatureSlug;
-import tech.jhipster.lite.generator.slug.domain.JHLiteModuleSlug;
 import tech.jhipster.lite.module.domain.resource.JHipsterModuleOrganization;
 import tech.jhipster.lite.module.domain.resource.JHipsterModuleResource;
 
@@ -14,10 +15,10 @@ class TikuiModuleConfiguration {
   @Bean
   JHipsterModuleResource tikuiModule(TikuiApplicationService tikui) {
     return JHipsterModuleResource.builder()
-      .slug(JHLiteModuleSlug.TIKUI)
+      .slug(TIKUI)
       .withoutProperties()
       .apiDoc("Frontend", "Add Tikui, a pattern library to build your styles")
-      .organization(JHipsterModuleOrganization.builder().addDependency(JHLiteFeatureSlug.CLIENT_CORE).build())
+      .organization(JHipsterModuleOrganization.builder().addDependency(CLIENT_CORE).build())
       .tags("client", "frontend", "tikui")
       .factory(tikui::buildModule);
   }

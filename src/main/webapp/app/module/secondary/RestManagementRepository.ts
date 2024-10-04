70b980333ef4385ac6d8d93f90ff4b142cbdea60
Quentin <quentin.monmert@gmail.com>
2024-10-04 23:00:37 +0200

    Sonar: Fields that are only assigned in the constructor should be readonly

--- src/main/webapp/app/module/secondary/RestManagementRepository.ts
+++ src/main/webapp/app/module/secondary/RestManagementRepository.ts
@@ -3,7 +3,7 @@ import { AxiosHttp } from '@/shared/http/infrastructure/secondary/AxiosHttp';
 import { ManagementInfo } from '../domain/ManagementInfo';
 
 export class RestManagementRepository implements ManagementRepository {
-  constructor(private axiosInstance: AxiosHttp) {}
+  constructor(private readonly axiosInstance: AxiosHttp) {}
 
   getInfo(): Promise<ManagementInfo> {
     return this.axiosInstance.get<ManagementInfo>('/management/info').then(res => res.data);

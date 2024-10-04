44a9844edcb6b144925d92edaf1034dffa0a920e
Renan Franca <renan.andradefranca@gmail.com>
2024-10-04 16:56:00 -0300

    ci: add vuejwtapp with the new vue-jwt module and cucumber_with_jwt in generate.sh

--- tests-ci/generate.sh
+++ tests-ci/generate.sh
@@ -411,6 +411,24 @@ elif [[ $application == 'reactapp' ]]; then
 
   applyModules "react-jwt"
 
+elif [[ $application == 'vuejwtapp' ]]; then
+  init_server
+  spring_boot_mvc
+  sonar_back_front
+
+  frontend_server_plugin
+  applyModules \
+    "typescript" \
+    "prettier" \
+    "vue-core" \
+    "vue-i18next" \
+    "vue-pinia" \
+    "vue-jwt" \
+    "playwright-component-tests" \
+    "cypress-e2e"
+
+  cucumber_with_jwt
+
 elif [[ $application == 'vueoauth2app' ]]; then
   init_server
   spring_boot_mvc

1f2d63382c4972c8aca3cb19a3c23f09a5263a15
Aurélien Mino <aurelien.mino@gmail.com>
2024-10-27 11:19:07 +0100

    feat(approval-tests): suppress warnings on PackageSettings

--- src/test/java/tech/jhipster/lite/PackageSettings.java
+++ src/test/java/tech/jhipster/lite/PackageSettings.java
@@ -1,11 +1,14 @@
 package tech.jhipster.lite;
 
 import org.approvaltests.core.ApprovalFailureReporter;
-import org.approvaltests.reporters.*;
+import org.approvaltests.reporters.AutoApproveWhenEmptyReporter;
+import org.approvaltests.reporters.DiffReporter;
+import org.approvaltests.reporters.FirstWorkingReporter;
 
 /**
  * ApprovalTests.Java configuration.
  * */
+@SuppressWarnings({ "unused", "java:S115" })
 public class PackageSettings {
 
   private static final String ApprovalBaseDirectory = "../resources";

3f9f93bdff232b55ba937d835c12c7d011139aa2
Mohamed <mohamed.aitabderrahman@gmail.com>
2024-11-10 20:02:27 +0100

    Add langchain4j to github actions ci

--- src/main/java/tech/jhipster/lite/generator/server/springboot/mvc/sample/langchain4j/domain/SampleLangChain4jModuleFactory.java
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/mvc/sample/langchain4j/domain/SampleLangChain4jModuleFactory.java
@@ -1,8 +1,6 @@
 package tech.jhipster.lite.generator.server.springboot.mvc.sample.langchain4j.domain;
 
-import static tech.jhipster.lite.module.domain.JHipsterModule.from;
-import static tech.jhipster.lite.module.domain.JHipsterModule.moduleBuilder;
-import static tech.jhipster.lite.module.domain.JHipsterModule.toSrcMainJava;
+import static tech.jhipster.lite.module.domain.JHipsterModule.*;
 
 import tech.jhipster.lite.module.domain.JHipsterModule;
 import tech.jhipster.lite.module.domain.file.JHipsterDestination;
@@ -23,12 +21,16 @@ public class SampleLangChain4jModuleFactory {
 
     String packagePath = properties.packagePath();
     JHipsterDestination mainDestination = toSrcMainJava().append(packagePath).append(SAMPLE);
+    JHipsterDestination testDestination = toSrcTestJava().append(packagePath).append(SAMPLE);
 
     //@formatter:off
     return moduleBuilder(properties)
       .files()
         .batch(SOURCE.append("main").append(PRIMARY), mainDestination.append(PRIMARY))
-          .addTemplate("ChatResource.java.mustache")
+          .addTemplate("ChatResource.java")
+          .and()
+        .batch(SOURCE.append("test").append(SAMPLE).append(PRIMARY), testDestination.append(PRIMARY))
+          .addTemplate("ChatResourceTest.java")
           .and()
         .and()
       .build();

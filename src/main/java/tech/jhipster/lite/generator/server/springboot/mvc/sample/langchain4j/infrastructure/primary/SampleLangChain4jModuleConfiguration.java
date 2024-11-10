86dcbfbbc1c717975ba68d5a8460df74e3f4d974
Mohamed <mohamed.aitabderrahman@gmail.com>
2024-11-10 17:07:40 +0100

    Generate LangChain4j sample

--- src/main/java/tech/jhipster/lite/generator/server/springboot/mvc/sample/langchain4j/infrastructure/primary/SampleLangChain4jModuleConfiguration.java
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/mvc/sample/langchain4j/infrastructure/primary/SampleLangChain4jModuleConfiguration.java
@@ -6,7 +6,6 @@ import static tech.jhipster.lite.shared.slug.domain.JHLiteModuleSlug.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tech.jhipster.lite.generator.server.springboot.mvc.sample.langchain4j.application.SampleLangChain4jApplicationService;
-import tech.jhipster.lite.generator.server.springboot.mvc.sample.langchain4j.domain.SampleLangChain4jModuleFactory;
 import tech.jhipster.lite.module.domain.resource.JHipsterModuleOrganization;
 import tech.jhipster.lite.module.domain.resource.JHipsterModulePropertiesDefinition;
 import tech.jhipster.lite.module.domain.resource.JHipsterModuleResource;
--- src/test/features/server/springboot/langchain4j.feature
+++ src/test/features/server/springboot/langchain4j.feature
@@ -8,3 +8,10 @@ Feature: LangChain4j module
     Then I should have entries in "src/main/resources/config/application.yml"
       | open-ai     |
       | langchain4j |
+
+  Scenario: Should add Spring Boot LangChain4j Sample
+    When I apply "spring-boot-langchain4j-sample" module to default project with maven file
+      | packageName | tech.jhipster.chips |
+      | baseName    | jhipster            |
+    Then I should have files in "src/main/java/tech/jhipster/chips/sample/infrastructure/primary"
+      | ChatResource.java |

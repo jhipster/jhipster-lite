056c6e6221304fa877b6c01b4035b2733fe0e556
Aurélien Mino <aurelien.mino@gmail.com>
2024-12-28 23:22:38 +0100

    chore: fix formatting

--- src/test/webapp/component/cypress-config.ts
+++ src/test/webapp/component/cypress-config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'cypress';
 import registerCodeCoverageTasks from '@cypress/code-coverage/task';
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import istanbul from 'vite-plugin-istanbul';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 import { defineConfig } from 'vite';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({

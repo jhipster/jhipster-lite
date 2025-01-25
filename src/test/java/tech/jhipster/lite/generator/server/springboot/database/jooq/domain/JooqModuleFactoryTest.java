1b9c7cba5dbd8cea99f9b7c1b07f553ec59792a8
Aurélien Mino <aurelien.mino@gmail.com>
2025-01-25 23:21:13 +0100

    test(jooq): verify generated jooq-codegen-maven configuration

--- src/test/java/tech/jhipster/lite/generator/server/springboot/database/jooq/domain/JooqModuleFactoryTest.java
+++ src/test/java/tech/jhipster/lite/generator/server/springboot/database/jooq/domain/JooqModuleFactoryTest.java
@@ -34,7 +34,42 @@ class JooqModuleFactoryTest {
             </dependency>
         """
       )
-      .containing("<artifactId>jooq-codegen-maven</artifactId>");
+      .containing(
+        """
+              <plugin>
+                <groupId>org.jooq</groupId>
+                <artifactId>jooq-codegen-maven</artifactId>
+                <version>${jooq.version}</version>
+                <executions>
+                  <execution>
+                    <id>jooq-codegen</id>
+                    <phase>generate-resources</phase>
+                    <goals>
+                      <goal>generate</goal>
+                    </goals>
+                  </execution>
+                </executions>
+                <configuration>
+                  <jdbc>
+                    <url>jdbc:postgresql://localhost:5432/myapp</url>
+                    <user>myapp</user>
+                    <password></password>
+                  </jdbc>
+                  <generator>
+                    <database>
+                      <name>org.jooq.meta.postgres.PostgresDatabase</name>
+                      <includes>.*</includes>
+                      <inputSchema>public</inputSchema>
+                    </database>
+                    <target>
+                      <packageName>org.jooq.codegen</packageName>
+                      <directory>target/generated-sources/jooq</directory>
+                    </target>
+                  </generator>
+                </configuration>
+              </plugin>
+        """
+      );
   }
 
   @Test
@@ -56,7 +91,42 @@ class JooqModuleFactoryTest {
             </dependency>
         """
       )
-      .containing("<artifactId>jooq-codegen-maven</artifactId>");
+      .containing(
+        """
+              <plugin>
+                <groupId>org.jooq</groupId>
+                <artifactId>jooq-codegen-maven</artifactId>
+                <version>${jooq.version}</version>
+                <executions>
+                  <execution>
+                    <id>jooq-codegen</id>
+                    <phase>generate-resources</phase>
+                    <goals>
+                      <goal>generate</goal>
+                    </goals>
+                  </execution>
+                </executions>
+                <configuration>
+                  <jdbc>
+                    <url>jdbc:mariadb://localhost:3306/myapp</url>
+                    <user>root</user>
+                    <password></password>
+                  </jdbc>
+                  <generator>
+                    <database>
+                      <name>org.jooq.meta.mariadb.MariaDBDatabase</name>
+                      <includes>.*</includes>
+                      <inputSchema>properties.projectBaseName().name()</inputSchema>
+                    </database>
+                    <target>
+                      <packageName>org.jooq.codegen</packageName>
+                      <directory>target/generated-sources/jooq</directory>
+                    </target>
+                  </generator>
+                </configuration>
+              </plugin>
+        """
+      );
   }
 
   @Test
@@ -78,7 +148,42 @@ class JooqModuleFactoryTest {
             </dependency>
         """
       )
-      .containing("<artifactId>jooq-codegen-maven</artifactId>");
+      .containing(
+        """
+              <plugin>
+                <groupId>org.jooq</groupId>
+                <artifactId>jooq-codegen-maven</artifactId>
+                <version>${jooq.version}</version>
+                <executions>
+                  <execution>
+                    <id>jooq-codegen</id>
+                    <phase>generate-resources</phase>
+                    <goals>
+                      <goal>generate</goal>
+                    </goals>
+                  </execution>
+                </executions>
+                <configuration>
+                  <jdbc>
+                    <url>jdbc:mysql://localhost:3306/myapp</url>
+                    <user>root</user>
+                    <password></password>
+                  </jdbc>
+                  <generator>
+                    <database>
+                      <name>org.jooq.meta.mysql.MySQLDatabase</name>
+                      <includes>.*</includes>
+                      <inputSchema>properties.projectBaseName().name()</inputSchema>
+                    </database>
+                    <target>
+                      <packageName>org.jooq.codegen</packageName>
+                      <directory>target/generated-sources/jooq</directory>
+                    </target>
+                  </generator>
+                </configuration>
+              </plugin>
+        """
+      );
   }
 
   @Test
@@ -100,6 +205,41 @@ class JooqModuleFactoryTest {
             </dependency>
         """
       )
-      .containing("<artifactId>jooq-codegen-maven</artifactId>");
+      .containing(
+        """
+              <plugin>
+                <groupId>org.jooq</groupId>
+                <artifactId>jooq-codegen-maven</artifactId>
+                <version>${jooq.version}</version>
+                <executions>
+                  <execution>
+                    <id>jooq-codegen</id>
+                    <phase>generate-resources</phase>
+                    <goals>
+                      <goal>generate</goal>
+                    </goals>
+                  </execution>
+                </executions>
+                <configuration>
+                  <jdbc>
+                    <url>jdbc:sqlserver://localhost:1433;database=myapp;trustServerCertificate=true</url>
+                    <user>SA</user>
+                    <password>yourStrong(!)Password</password>
+                  </jdbc>
+                  <generator>
+                    <database>
+                      <name>org.jooq.meta.sqlserver.SQLServerDatabase</name>
+                      <includes>.*</includes>
+                      <inputSchema>model</inputSchema>
+                    </database>
+                    <target>
+                      <packageName>org.jooq.codegen</packageName>
+                      <directory>target/generated-sources/jooq</directory>
+                    </target>
+                  </generator>
+                </configuration>
+              </plugin>
+        """
+      );
   }
 }

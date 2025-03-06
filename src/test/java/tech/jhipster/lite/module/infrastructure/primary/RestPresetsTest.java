d229c6b738771208df7dcc88e92beada8ec79573
Renan Franca <renan.andradefranca@gmail.com>
2025-03-06 09:23:45 -0300

    refactor: use Presets value object as a parameter instead of passing its value directly

--- src/test/java/tech/jhipster/lite/module/infrastructure/primary/RestPresetsTest.java
+++ src/test/java/tech/jhipster/lite/module/infrastructure/primary/RestPresetsTest.java
@@ -2,7 +2,6 @@ package tech.jhipster.lite.module.infrastructure.primary;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Collection;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import tech.jhipster.lite.JsonHelper;
@@ -11,6 +10,7 @@ import tech.jhipster.lite.module.domain.JHipsterModuleSlug;
 import tech.jhipster.lite.module.domain.JHipsterModuleSlugs;
 import tech.jhipster.lite.module.domain.preset.Preset;
 import tech.jhipster.lite.module.domain.preset.PresetName;
+import tech.jhipster.lite.module.domain.preset.Presets;
 
 @UnitTest
 class RestPresetsTest {
@@ -20,15 +20,17 @@ class RestPresetsTest {
     assertThat(JsonHelper.writeAsString(RestPresets.from(presets()))).isEqualTo(json());
   }
 
-  private static Collection<Preset> presets() {
-    return List.of(
-      new Preset(
-        new PresetName("test preset one"),
-        new JHipsterModuleSlugs(List.of(new JHipsterModuleSlug("test-module-one"), new JHipsterModuleSlug("test-module-two")))
-      ),
-      new Preset(
-        new PresetName("test preset two"),
-        new JHipsterModuleSlugs(List.of(new JHipsterModuleSlug("test-module-three"), new JHipsterModuleSlug("test-module-four")))
+  private static Presets presets() {
+    return new Presets(
+      List.of(
+        new Preset(
+          new PresetName("test preset one"),
+          new JHipsterModuleSlugs(List.of(new JHipsterModuleSlug("test-module-one"), new JHipsterModuleSlug("test-module-two")))
+        ),
+        new Preset(
+          new PresetName("test preset two"),
+          new JHipsterModuleSlugs(List.of(new JHipsterModuleSlug("test-module-three"), new JHipsterModuleSlug("test-module-four")))
+        )
       )
     );
   }

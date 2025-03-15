10c6f979983ee55c5e4f43a8849639b4479a6912
Quentin <quentin.monmert@gmail.com>
2025-03-15 16:40:51 +0100

    Rename SpringDoc to Springdoc

--- src/main/java/tech/jhipster/lite/generator/server/springboot/apidocumentation/springdocoauth/application/SpringdocOauth2ApplicationService.java
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/apidocumentation/springdocoauth/application/SpringdocOauth2ApplicationService.java
@@ -1,17 +1,17 @@
 package tech.jhipster.lite.generator.server.springboot.apidocumentation.springdocoauth.application;
 
 import org.springframework.stereotype.Service;
-import tech.jhipster.lite.generator.server.springboot.apidocumentation.springdocoauth.domain.SpringDocOauth2ModuleFactory;
+import tech.jhipster.lite.generator.server.springboot.apidocumentation.springdocoauth.domain.SpringdocOauth2ModuleFactory;
 import tech.jhipster.lite.module.domain.JHipsterModule;
 import tech.jhipster.lite.module.domain.properties.JHipsterModuleProperties;
 
 @Service
 public class SpringdocOauth2ApplicationService {
 
-  private final SpringDocOauth2ModuleFactory factory;
+  private final SpringdocOauth2ModuleFactory factory;
 
   public SpringdocOauth2ApplicationService() {
-    factory = new SpringDocOauth2ModuleFactory();
+    factory = new SpringdocOauth2ModuleFactory();
   }
 
   public JHipsterModule buildModule(JHipsterModuleProperties properties) {
--- src/main/java/tech/jhipster/lite/generator/server/springboot/apidocumentation/springdocokta/application/SpringdocOktaApplicationService.java
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/apidocumentation/springdocokta/application/SpringdocOktaApplicationService.java
@@ -1,17 +1,17 @@
 package tech.jhipster.lite.generator.server.springboot.apidocumentation.springdocokta.application;
 
 import org.springframework.stereotype.Service;
-import tech.jhipster.lite.generator.server.springboot.apidocumentation.springdocokta.domain.SpringDocOktaModuleFactory;
+import tech.jhipster.lite.generator.server.springboot.apidocumentation.springdocokta.domain.SpringdocOktaModuleFactory;
 import tech.jhipster.lite.module.domain.JHipsterModule;
 import tech.jhipster.lite.module.domain.properties.JHipsterModuleProperties;
 
 @Service
-public class SpringDocOktaApplicationService {
+public class SpringdocOktaApplicationService {
 
-  private final SpringDocOktaModuleFactory factory;
+  private final SpringdocOktaModuleFactory factory;
 
-  public SpringDocOktaApplicationService() {
-    factory = new SpringDocOktaModuleFactory();
+  public SpringdocOktaApplicationService() {
+    factory = new SpringdocOktaModuleFactory();
   }
 
   public JHipsterModule buildModule(JHipsterModuleProperties properties) {

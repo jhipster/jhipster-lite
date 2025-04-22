332216b39e2b7a0f5fe6b0aaf063ee3b73730b41
Aurélien Mino <aurelien.mino@gmail.com>
2025-04-22 23:30:31 +0200

    feat(cucumber): replace test selection using cucumber.features by @SelectDirectories This fixes the warning during execution "WARNING: Discovering tests using the cucumber.features property. [...] If you are using the JUnit 5 Suite Engine, Platform Launcher API or Console Launcher you should not use this property."

--- src/test/java/tech/jhipster/lite/cucumber/CucumberTest.java
+++ src/test/java/tech/jhipster/lite/cucumber/CucumberTest.java
@@ -4,6 +4,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectDirectories;
 import org.junit.platform.suite.api.Suite;
 import tech.jhipster.lite.ComponentTest;
 
@@ -11,10 +12,10 @@ import tech.jhipster.lite.ComponentTest;
 @ComponentTest
 @IncludeEngines("cucumber")
 @SuppressWarnings("java:S2187")
+@SelectDirectories("src/test/features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "tech.jhipster.lite")
 @ConfigurationParameter(
   key = PLUGIN_PROPERTY_NAME,
   value = "pretty, json:target/cucumber/cucumber.json, html:target/cucumber/cucumber.htm, junit:target/cucumber/TEST-cucumber.xml"
 )
-@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/features")
 public class CucumberTest {}

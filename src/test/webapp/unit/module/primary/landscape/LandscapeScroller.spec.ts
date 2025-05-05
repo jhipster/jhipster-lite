bc771a4d202903dd6bfe60cc002660b514207b21
Quentin <quentin.monmert@gmail.com>
2025-05-05 15:47:09 +0200

    Factorize HTMLElementStub

--- src/test/webapp/unit/module/primary/landscape/LandscapeScroller.spec.ts
+++ src/test/webapp/unit/module/primary/landscape/LandscapeScroller.spec.ts
@@ -13,30 +13,26 @@ const stubHtmlElement = (): HTMLElementStub =>
 
 describe('LandscapeScroller', () => {
   let landscapeScroller: LandscapeScroller;
+  let element: HTMLElementStub;
 
   beforeEach(() => {
     landscapeScroller = new LandscapeScroller();
+    element = stubHtmlElement();
   });
 
   it('should scroll', () => {
-    const element = stubHtmlElement();
-
     landscapeScroller.scroll(element, 10, 10);
 
     expect(element.scroll).toHaveBeenCalledExactlyOnceWith(10, 10);
   });
 
   it('should scroll smooth', () => {
-    const element = stubHtmlElement();
-
     landscapeScroller.scrollSmooth(element, 10, 10);
 
     expect(element.scroll).toHaveBeenCalledExactlyOnceWith({ left: 10, top: 10, behavior: 'smooth' });
   });
 
   it('should scroll into view', () => {
-    const element = stubHtmlElement();
-
     landscapeScroller.scrollIntoView(element);
 
     expect(element.scrollIntoView).toHaveBeenCalledExactlyOnceWith({ behavior: 'smooth', block: 'center', inline: 'center' });

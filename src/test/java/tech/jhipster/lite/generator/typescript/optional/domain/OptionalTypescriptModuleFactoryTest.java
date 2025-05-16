98594981a3bb29f68c17cc9eb89a2baeda184f5d
Quentin <quentin.monmert@gmail.com>
2025-05-16 21:03:17 +0200

    Fix packages

--- src/test/java/tech/jhipster/lite/generator/typescript/optional/domain/OptionalTypescriptModuleFactoryTest.java
+++ src/test/java/tech/jhipster/lite/generator/typescript/optional/domain/OptionalTypescriptModuleFactoryTest.java
@@ -1,11 +1,10 @@
-package tech.jhipster.lite.generator.typescript.domain.optional;
+package tech.jhipster.lite.generator.typescript.optional.domain;
 
 import static tech.jhipster.lite.module.infrastructure.secondary.JHipsterModulesAssertions.assertThatModuleWithFiles;
 
 import org.junit.jupiter.api.Test;
 import tech.jhipster.lite.TestFileUtils;
 import tech.jhipster.lite.UnitTest;
-import tech.jhipster.lite.generator.typescript.optional.domain.OptionalTypescriptModuleFactory;
 import tech.jhipster.lite.module.domain.JHipsterModule;
 import tech.jhipster.lite.module.domain.JHipsterModulesFixture;
 import tech.jhipster.lite.module.domain.properties.JHipsterModuleProperties;

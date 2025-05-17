16b03aa61b06f7f68ce04466809ddfc8068b9c1e
Quentin <quentin.monmert@gmail.com>
2025-05-17 15:41:34 +0200

    Rework Thymeleaf module

--- src/main/java/tech/jhipster/lite/generator/server/springboot/thymeleaf/init/application/SpringBootThymeleafApplicationService.java
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/thymeleaf/init/application/SpringBootThymeleafApplicationService.java
@@ -1,7 +1,7 @@
-package tech.jhipster.lite.generator.server.springboot.thymeleaf.application;
+package tech.jhipster.lite.generator.server.springboot.thymeleaf.init.application;
 
 import org.springframework.stereotype.Service;
-import tech.jhipster.lite.generator.server.springboot.thymeleaf.domain.SpringBootThymeleafModuleFactory;
+import tech.jhipster.lite.generator.server.springboot.thymeleaf.init.domain.SpringBootThymeleafModuleFactory;
 import tech.jhipster.lite.module.domain.JHipsterModule;
 import tech.jhipster.lite.module.domain.properties.JHipsterModuleProperties;
 
--- src/main/java/tech/jhipster/lite/generator/server/springboot/thymeleaf/init/infrastructure/primary/SpringBootThymeleafModuleConfiguration.java
+++ src/main/java/tech/jhipster/lite/generator/server/springboot/thymeleaf/init/infrastructure/primary/SpringBootThymeleafModuleConfiguration.java
@@ -1,11 +1,11 @@
-package tech.jhipster.lite.generator.server.springboot.thymeleaf.infrastructure.primary;
+package tech.jhipster.lite.generator.server.springboot.thymeleaf.init.infrastructure.primary;
 
 import static tech.jhipster.lite.shared.slug.domain.JHLiteFeatureSlug.SPRING_SERVER;
 import static tech.jhipster.lite.shared.slug.domain.JHLiteModuleSlug.SPRING_BOOT_THYMELEAF;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tech.jhipster.lite.generator.server.springboot.thymeleaf.application.SpringBootThymeleafApplicationService;
+import tech.jhipster.lite.generator.server.springboot.thymeleaf.init.application.SpringBootThymeleafApplicationService;
 import tech.jhipster.lite.module.domain.resource.JHipsterModuleOrganization;
 import tech.jhipster.lite.module.domain.resource.JHipsterModulePropertiesDefinition;
 import tech.jhipster.lite.module.domain.resource.JHipsterModuleResource;

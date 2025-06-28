9dbd8438e84424e51b46ea2a3054632d2dba8b66
Aurélien Mino <aurelien.mino@gmail.com>
2025-06-28 15:33:23 +0200

    ci: simplify usage of generate.sh It's possible to only specify target application, other parameters will take default values

--- tests-ci/generate.sh
+++ tests-ci/generate.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
 show_syntax() {
-  echo "Usage: $0 <application> <java-build-tool> <spring-configuration-format> <node-package-manager>" >&2
+  echo "Usage: $0 <application> [<java-build-tool> <spring-configuration-format> <node-package-manager>]" >&2
   exit 1
 }
 
-if [ "$#" -ne 4 ]; then
+if [ "$#" -eq 1 ]; then
+  application=$1
+  java_build_tool=maven
+  configuration_format=yaml
+  node_package_manager=npm
+elif [ "$#" -eq 4 ]; then
+  application=$1
+  java_build_tool=$2
+  configuration_format=$3
+  node_package_manager=$4
+else
   show_syntax
 fi
 
@@ -14,11 +24,6 @@ if test -f "modulePayload.json"; then
 elif test -f tests-ci/modulePayload.json; then
   payloadFile=tests-ci/modulePayload.json
 fi
-
-application=$1
-java_build_tool=$2
-configuration_format=$3
-node_package_manager=$4
 
 apply_modules() {
   for module in $@; do
